0b52ad5876fef0a7ce73351434146fee6c202e33
Kamil Wojas <107795476+KamilWojas@users.noreply.github.com>
2024-11-07 23:40:13 +0100

    Add files via upload

---
+++ PetlaForEach/Main.java
@@ -0,0 +1,13 @@
+package PetlaForEach;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] names = {"Kamil", "Kaska", "Bastek"};
+
+        for (int i = 0; i < names.length; i++){
+            System.out.println(names[i]);
+
+        }
+    }
+}
