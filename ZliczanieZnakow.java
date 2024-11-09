c71efb6f15d8bda689dd4d7ed200bc0864acf301
Kamil Wojas <107795476+KamilWojas@users.noreply.github.com>
2024-11-09 23:35:27 +0100

    Add files via upload

---
+++ ZliczanieZnakow.java
@@ -0,0 +1,26 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ZliczanieZnakow {
+    public static void main(String[] args) {
+        String sciezkaDoPliku = "sciezka_do_twojego_pliku.txt";
+
+        Map<Character, Integer> mapaZnakow = new HashMap<>();
+
+        try (FileInputStream fileInputStream = new FileInputStream(sciezkaDoPliku)) {
+            int odczytanyBajt;
+            while ((odczytanyBajt = fileInputStream.read()) != -1) {
+                char znak = (char) odczytanyBajt;
+                mapaZnakow.put(znak, mapaZnakow.getOrDefault(znak, 0) + 1);
+            }
+        } catch (IOException e) {
+            System.err.println("Błąd podczas odczytu pliku: " + e.getMessage());
+        }
+
+        for (Map.Entry<Character, Integer> entry : mapaZnakow.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
