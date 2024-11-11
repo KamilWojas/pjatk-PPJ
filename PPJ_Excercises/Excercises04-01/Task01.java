b08d7b2d627bed1072136b19ab6ae65957ce8ce1
KamilWojas <wojaskamil@yahoo.pl>
2024-11-11 21:23:51 +0100

    Commit2

---
+++ PPJ_Excercises/Excercises04-01/Task01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task0503 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj znak");
+        char znak = scanner.next().charAt(0);
+
+
+        if (znak == 'a' || znak == 'ą') {
+            System.out.println("To jest samogłoska ");
+        }else
+            System.out.println("To nie jest samogłoska");
+
+    }
+
+}
