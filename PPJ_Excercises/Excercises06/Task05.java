a765dead46fb1d38e956adf8d0fe9ab63087390b
KamilWojas <wojaskamil@yahoo.pl>
2024-11-15 23:33:33 +0100

    ppj_excercises

---
+++ PPJ_Excercises/Excercises06/Task05.java
@@ -0,0 +1,13 @@
+
+public class Task05 {
+    public static void main(String[] args) {
+        int tab[];
+        System.out.println(tab);
+
+
+        int tab[] = null;
+        System.out.println(tab);
+
+    }
+}
+
