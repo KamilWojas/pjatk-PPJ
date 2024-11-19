871ee99e438278fab57af0bd6e639ce4199e0cdf
KamilWojas <wojaskamil@yahoo.pl>
2024-11-19 23:19:01 +0100

    ppj_excercises

---
+++ PPJ_Excercises/Excercises07/Task05.java
@@ -0,0 +1,28 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    char[][] chars = new char['z' - 'a']['z' - 'a'];
+
+        for(
+    int i = 0;
+    i<chars.length;i++)
+
+    {
+        char c = 'a';
+        for (int j = 0; j < chars.length; j++, c++) {
+            chars[i][j] = c;
+        }
+    }
+
+        for(
+    int i = 0, step = 1;
+    i<chars.length;i++)
+
+    {
+        System.out.println(chars[i][step]);
+        step = (step << 1) % chars[i].length;
+    }
+}
