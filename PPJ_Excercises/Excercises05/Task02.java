61f0c2adc1215d34ccde245691bc95ebea0cf4f6
KamilWojas <wojaskamil@yahoo.pl>
2024-11-20 23:31:10 +0100

    ppj_excercises

---
+++ PPJ_Excercises/Excercises05/Task02.java
@@ -0,0 +1,15 @@
+
+public class Task01 {
+    public static void main(String[] args) {
+
+    }
+
+    int s = 0;
+    int i = 1; // Inicjalizacja zmiennej sterującej
+        while(i <=10)
+
+    {
+        s = s + i; // Dodawanie kolejnej wartości do sumy
+        i++;       // Inkrementacja zmiennej sterującej
+    }
+}
