570dfafc4200491b3b831a672ac63b1e16e0bea6
KamilWojas <wojaskamil@yahoo.pl>
2024-11-21 23:59:23 +0100

    ppj_excercises

---
+++ PPJ_Excercises/Excercises05/Task06.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Liczby podzielne bez reszty przez 3 i 2 z przedziału od -1500 do 1500:");
+
+        for (int i = -1500; i <= 1500; i++) {
+            if (i % 3 == 0 && i % 2 == 0) { // Sprawdzenie podzielności przez 3 i 2
+                System.out.println(i);
+            }
+        }
+    }
+}
