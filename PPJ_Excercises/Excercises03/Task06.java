9a379c1d556a5a2419b7912d8a415b7da61c7057
KamilWojas <wojaskamil@yahoo.pl>
2024-12-01 00:06:40 +0100

    PPJ_Excercises

---
+++ PPJ_Excercises/Excercises03/Task06.java
@@ -0,0 +1,19 @@
+public class Task06 {
+    public static void main(String[] args) {
+        /*
+
+         Program skompiluje się poprawnie, ponieważ Java rozróżnia wielkość liter
+         w nazwach zmiennych. Dlatego `charValue` i `charvalue` są traktowane jako
+         dwie różne zmienne. Oczekuję, że obie zmienne zostaną poprawnie zainicjowane
+         i ich wartości zostaną wyświetlone na konsoli.
+         */
+
+        // Deklaracja i inicjalizacja zmiennych typu char
+        char charValue = 'A';
+        char charvalue = 'B';
+
+        // Wyświetlenie wartości zmiennych na konsoli
+        System.out.println("Wartość zmiennej charValue: " + charValue);
+        System.out.println("Wartość zmiennej charvalue: " + charvalue);
+    }
+}
