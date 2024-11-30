7b309ef2c037a0c24f61946a20e9c12e64086d3a
KamilWojas <wojaskamil@yahoo.pl>
2024-11-30 23:22:15 +0100

    PPJ_Excercises

---
+++ PPJ_Excercises/Excercises03/Task04.java
@@ -0,0 +1,9 @@
+public class Task04 {
+    public static void main(String[] args) {
+        // Deklaracja i inicjalizacja zmiennej typu int
+        int myNumber = 42;
+
+        // Wyświetlenie wartości zmiennej na konsoli
+        System.out.println("Wartość zmiennej myNumber: " + myNumber);
+    }
+}
