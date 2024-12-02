e93d3f53a47af90132ca44b7b1ce58d4a9c39c59
KamilWojas <wojaskamil@yahoo.pl>
2024-12-02 23:40:57 +0100

    PPJ_Excercises

---
+++ PPJ_Excercises/Excercises09/Osoba.java
@@ -0,0 +1,21 @@
+// Klasa Osoba
+public class Osoba {
+    public String imie;
+    public String nazwisko;
+    public int rokUrodzenia;
+
+    public static void main(String[] args) {
+        // Tworzenie obiektu klasy Osoba
+        Osoba osoba = new Osoba();
+
+        // Przypisanie wartości do pól
+        osoba.imie = "Jan";
+        osoba.nazwisko = "Kowalski";
+        osoba.rokUrodzenia = 1990;
+
+        // Wyświetlenie wartości pól
+        System.out.println("Imię: " + osoba.imie);
+        System.out.println("Nazwisko: " + osoba.nazwisko);
+        System.out.println("Rok urodzenia: " + osoba.rokUrodzenia);
+    }
+}
