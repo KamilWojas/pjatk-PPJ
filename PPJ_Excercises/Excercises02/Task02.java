324343e19e0166065b22c8a97f93b0e77a30b1da
KamilWojas <wojaskamil@yahoo.pl>
2024-12-04 23:10:55 +0100

    PPJ_Excercises

---
+++ PPJ_Excercises/Excercises02/Task02.java
@@ -0,0 +1,19 @@
+public class BitShiftExample {
+    public static void main(String[] args) {
+        // Wartość początkowa
+        int value = 14;
+        System.out.println("Wartość początkowa: " + value);
+
+        // Przesunięcie o 0 pozycji w prawo
+        int shiftRight0 = value >> 0;
+        System.out.println("Po przesunięciu o 0 pozycji w prawo: " + shiftRight0);
+
+        // Przesunięcie rezultatu o 2 pozycje w lewo
+        int shiftLeft2 = shiftRight0 << 2;
+        System.out.println("Po przesunięciu o 2 pozycje w lewo: " + shiftLeft2);
+
+        // Przesunięcie rezultatu o 4 pozycje w prawo
+        int shiftRight4 = shiftLeft2 >> 4;
+        System.out.println("Po przesunięciu o 4 pozycje w prawo: " + shiftRight4);
+    }
+}
