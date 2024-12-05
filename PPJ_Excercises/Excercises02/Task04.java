52f9ff80960cfa7f237e903bed0e3ad469893ba5
KamilWojas <wojaskamil@yahoo.pl>
2024-12-05 23:14:35 +0100

    PPJ_Excercises

--- PPJ_Excercises/Excercises02/Task04.java
+++ PPJ_Excercises/Excercises02/Task04.java
@@ -15,3 +15,26 @@ public class BitwiseOperations {
         System.out.println(a + " " + b + " " + c);
     }
 }
+/*
+Obliczenia dla a:
+a1 = 0x6D = 109 (w systemie dziesiętnym).
+a2 = 0x0D = 13 (w systemie dziesiętnym).
+Operacja AND (&) między 109 i 13:
+W bitach: 01101101 & 00001101 = 00001101.
+Wynik: a = 13.
+
+Obliczenia dla b:
+b1 = 0xA = 10 (w systemie dziesiętnym).
+b2 = 0xF = 15 (w systemie dziesiętnym).
+Operacja OR (|) między 10 i 15:
+W bitach: 00001010 | 00001111 = 00001111.
+Wynik: b = 15.
+
+Obliczenia dla c:
+c1 = 11 << 2 = 44 (przesunięcie bitowe w lewo o 2 pozycje to mnożenie przez 4).
+c2 = 6.
+Operacja XOR (^) między 44 i 6:
+W bitach: 00101100 ^ 00000110 = 00101010.
+Wynik: c = 42.
+
+ */
