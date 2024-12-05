7029f8f62fde9ce755ddc4d5a5d7a248dfeb9b6a
KamilWojas <wojaskamil@yahoo.pl>
2024-12-05 23:17:33 +0100

    PPJ_Excercises

--- PPJ_Excercises/Excercises02/Task02.java
+++ PPJ_Excercises/Excercises02/Task02.java
@@ -19,6 +19,20 @@ public class Task02 {
 }
 
 /*
+Pierwszy krok:
+
+>> 0: Przesunięcie bitowe w prawo o 0 pozycji nie zmienia wartości liczby.
+Drugi krok:
+
+<< 2: Przesunięcie bitowe w lewo o 2 pozycje powoduje pomnożenie liczby przez 2^2 (czyli 4).
+Trzeci krok:
+
+>> 4: Przesunięcie bitowe w prawo o 4 pozycje dzieli liczbę przez 2^4 (czyli 16), ignorując część ułamkową.
+ */
+
+
+
+/*
 Wartość początkowa: 14
 Po przesunięciu o 0 pozycji w prawo: 14
 Po przesunięciu o 2 pozycje w lewo: 56
