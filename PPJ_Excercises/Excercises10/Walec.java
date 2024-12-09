5ad64e028ac8421cd09b890ddb01d175d648022a
KamilWojas <wojaskamil@yahoo.pl>
2024-12-09 23:14:33 +0100

    PPJ_Excercises

---
+++ PPJ_Excercises/Excercises10/Walec.java
@@ -0,0 +1,24 @@
+public class Walec {
+    private double promien;
+    private double wysokosc;
+
+    // Konstruktor
+    public Walec(double promien, double wysokosc) {
+        this.promien = promien;
+        this.wysokosc = wysokosc;
+    }
+
+    // Metoda show wyświetlająca pole powierzchni podstawy i objętość walca
+    public void show() {
+        double polePodstawy = Math.PI * promien * promien; // Pole podstawy walca
+        double objetoscWalca = polePodstawy * wysokosc; // Objętość walca
+        System.out.println("Pole powierzchni podstawy: " + polePodstawy);
+        System.out.println("Objętość walca: " + objetoscWalca);
+    }
+
+    // Przykład użycia klasy
+    public static void main(String[] args) {
+        Walec walec = new Walec(3.0, 5.0); // Tworzymy walec o promieniu 3 i wysokości 5
+        walec.show();
+    }
+}
