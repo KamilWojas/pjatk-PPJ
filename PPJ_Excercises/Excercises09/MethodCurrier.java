730155aac4aec4dbde7a0fc8f71458ba0787d565
KamilWojas <wojaskamil@yahoo.pl>
2024-12-10 23:51:10 +0100

    PPJ_Excercises

--- PPJ_Excercises/Excercises09/MethodCurrier.java
+++ PPJ_Excercises/Excercises09/MethodCurrier.java
@@ -21,3 +21,10 @@ public class MethodCurrier {
         methodCurrier.setValue((byte) 5);    // byte
     }
 }
+
+/*
+Dla int: Wywoływana jest metoda setValue(int), ponieważ przekazany literał 10 jest typu int domyślnie.
+Dla float: Wywoływana jest metoda setValue(float), ponieważ literał 10.5f ma przyrostek f, oznaczający typ float.
+Dla char: Kompilator automatycznie promuje char do int (unicode znaku), więc metoda setValue(int) zostanie wywołana.
+Dla byte: Literał (byte) 5 jest explicite rzutowany na byte, ale byte jest automatycznie promowany do int, więc metoda setValue(int) zostanie wywołana.
+ */
