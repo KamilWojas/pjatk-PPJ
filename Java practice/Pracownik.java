266f8fa31d49844d59a7a6e61ffea2fd06450b2c
KamilWojas <wojaskamil@yahoo.pl>
2024-12-15 00:37:11 +0100

    PPJ_Excercises

---
+++ Java practice/Pracownik.java
@@ -0,0 +1,77 @@
+// Klasa bazowa Pracownik
+class Pracownik {
+    protected String imię;
+    protected String nazwisko;
+    protected double wynagrodzeniePodstawowe;
+
+    // Konstruktor
+    public Pracownik(String imię, String nazwisko, double wynagrodzeniePodstawowe) {
+        this.imię = imię;
+        this.nazwisko = nazwisko;
+        this.wynagrodzeniePodstawowe = wynagrodzeniePodstawowe;
+    }
+
+    // Metoda opisująca pracownika
+    public String opis() {
+        return "Imię: " + imię + ", Nazwisko: " + nazwisko + ", Wynagrodzenie podstawowe: " + wynagrodzeniePodstawowe;
+    }
+}
+
+// Klasa PracownikEtatowy dziedzicząca po Pracownik
+class PracownikEtatowy extends Pracownik {
+    private double premia;
+
+    // Konstruktor
+    public PracownikEtatowy(String imię, String nazwisko, double wynagrodzeniePodstawowe, double premia) {
+        super(imię, nazwisko, wynagrodzeniePodstawowe); // Wywołanie konstruktora klasy bazowej
+        this.premia = premia;
+    }
+
+    // Nadpisanie metody opis
+    @Override
+    public String opis() {
+        return super.opis() + ", Premia: " + premia + ", Pełne wynagrodzenie: " + (wynagrodzeniePodstawowe + premia);
+    }
+}
+
+// Klasa PracownikZlecenie dziedzicząca po Pracownik
+class PracownikZlecenie extends Pracownik {
+    private int liczbaGodzin;
+    private double stawkaZaGodzinę;
+
+    // Konstruktor
+    public PracownikZlecenie(String imię, String nazwisko, double wynagrodzeniePodstawowe, int liczbaGodzin, double stawkaZaGodzinę) {
+        super(imię, nazwisko, wynagrodzeniePodstawowe); // Wywołanie konstruktora klasy bazowej
+        this.liczbaGodzin = liczbaGodzin;
+        this.stawkaZaGodzinę = stawkaZaGodzinę;
+    }
+
+    // Nadpisanie metody opis
+    @Override
+    public String opis() {
+        double wynagrodzenieCałkowite = liczbaGodzin * stawkaZaGodzinę;
+        return "Imię: " + imię + ", Nazwisko: " + nazwisko + ", Wynagrodzenie całkowite: " + wynagrodzenieCałkowite;
+    }
+}
+
+// Klasa główna
+import java.util.ArrayList;
+        import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // Tworzenie obiektów
+        Pracownik etatowy = new PracownikEtatowy("Jan", "Kowalski", 4000, 1000);
+        Pracownik zlecenie = new PracownikZlecenie("Anna", "Nowak", 0, 120, 20);
+
+        // Tworzenie listy pracowników
+        List<Pracownik> pracownicy = new ArrayList<>();
+        pracownicy.add(etatowy);
+        pracownicy.add(zlecenie);
+
+        // Wyświetlanie opisów pracowników
+        for (Pracownik p : pracownicy) {
+            System.out.println(p.opis());
+        }
+    }
+}
