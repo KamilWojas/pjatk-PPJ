0a461dbfd8f204dd019e359549fb71316268cd55
KamilWojas <wojaskamil@yahoo.pl>
2024-12-16 23:10:06 +0100

    PPJ_Excercises

--- PPJ_Excercises/Excercises11/Animal.java
+++ PPJ_Excercises/Excercises11/Animal.java
@@ -1,1 +1,15 @@
+// Klasa bazowa Animal
+class Animal {
+    protected String species;
 
+
+
+    p
+    public Animal(String species) {
+        this.species = species;
+    }
+
+    public void makeSound() {
+        System.out.println("Unknown");
+    }
+}
