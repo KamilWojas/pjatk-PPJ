a80f5e2007ca37bfa5f89808c937775d6e552dd9
KamilWojas <wojaskamil@yahoo.pl>
2024-12-17 23:12:03 +0100

    PPJ_Excercises

--- PPJ_Excercises/Excercises11/Owoc.java
+++ PPJ_Excercises/Excercises11/Owoc.java
@@ -18,3 +18,33 @@ class Owoc {
         return masa;
     }
 }
+// Klasy dziedziczące: Jablko, Pomarancza, Gruszka
+class Jablko extends Owoc {
+    public Jablko() {
+        super("Jablko", losujMase());
+    }
+
+    private static double losujMase() {
+        return 100 + new Random().nextInt(151); // Masa od 100 do 250 g
+    }
+}
+
+class Pomarancza extends Owoc {
+    public Pomarancza() {
+        super("Pomarańcza", losujMase());
+    }
+
+    private static double losujMase() {
+        return 100 + new Random().nextInt(151); // Masa od 100 do 250 g
+    }
+}
+
+class Gruszka extends Owoc {
+    public Gruszka() {
+        super("Gruszka", losujMase());
+    }
+
+    private static double losujMase() {
+        return 100 + new Random().nextInt(151); // Masa od 100 do 250 g
+    }
+}
