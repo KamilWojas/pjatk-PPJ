a055101e497ea3356c9528e28c6c6534c4b48902
KamilWojas <wojaskamil@yahoo.pl>
2024-12-17 23:12:39 +0100

    PPJ_Excercises

--- PPJ_Excercises/Excercises11/Animal.java
+++ PPJ_Excercises/Excercises11/Animal.java
@@ -13,3 +13,28 @@ class Animal {
         System.out.println("Unknown");
     }
 }
+// Klasa Cat dziedzicząca po Animal
+class Cat extends Animal {
+
+    pub
+    public Cat() {
+        super("Cat");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+}
+
+// Klasa Dog dziedzicząca po Animal
+class Dog extends Animal {
+    public Dog() {
+        super("Dog");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Woof");
+    }
+}
