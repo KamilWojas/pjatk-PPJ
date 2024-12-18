8bfa179e8059e9eb1f217ff3b9b4c6b331504062
KamilWojas <wojaskamil@yahoo.pl>
2024-12-18 23:54:06 +0100

    PPJ_Excercises

--- PPJ_Petle/PetlaFor/ZadaniePetleSuma.java
+++ PPJ_Petle/PetlaFor/ZadaniePetleSuma.java
@@ -2,3 +2,7 @@
 Napisz program, który obliczy sumę wszystkich par
 liczb z tablicy. Wykorzystaj do tego dwie zagnieżdżone pętle for.
  */
+
+public class ZadaniePetleSuma {
+    public static void main(String[] args) {
+        int[] tab = {2, 4, 6, 8}; // Tablica liczb całkowitych
