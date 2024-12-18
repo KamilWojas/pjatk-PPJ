d5c0c3ccb118e11708df7c789bdf869324aef434
KamilWojas <wojaskamil@yahoo.pl>
2024-12-18 23:56:02 +0100

    PPJ_Excercises

--- PPJ_Petle/PetlaFor/ZadaniePetleSuma.java
+++ PPJ_Petle/PetlaFor/ZadaniePetleSuma.java
@@ -28,4 +28,8 @@ Suma tab[1] i tab[3]: 12
 Suma tab[2] i tab[2]: 12
 Suma tab[2] i tab[3]: 14
 Suma tab[3] i tab[3]: 16
+
+Pętla zewnętrzna przechodzi po każdym elemencie tablicy.
+Pętla wewnętrzna oblicza sumy bieżącego elementu z wszystkimi następnymi elementami tablicy.
+Dzięki temu wyświetlamy sumy dla wszystkich możliwych kombinacji elementów z tablicy (włącznie z elementem dodanym do siebie samego).
  */
