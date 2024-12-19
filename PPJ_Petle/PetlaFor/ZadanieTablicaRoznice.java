28e4cc9b1158e291a98b7d4f4e229089629c13ad
KamilWojas <wojaskamil@yahoo.pl>
2024-12-19 23:42:36 +0100

    PPJ_Excercises

--- PPJ_Petle/PetlaFor/ZadanieTablicaRoznice.java
+++ PPJ_Petle/PetlaFor/ZadanieTablicaRoznice.java
@@ -1,3 +1,5 @@
 /*
-Napisz program, który obliczy różnicę każdego elementu tablicy z sumą wszystkich pozostałych elementów w tablicy. Wynik dla każdego elementu wyświetl na konsoli. Wykorzystaj jedną pętlę for.
+Napisz program, który obliczy różnicę każdego elementu tablicy z sumą wszystkich
+pozostałych elementów w tablicy. Wynik dla każdego elementu wyświetl na konsoli.
+Wykorzystaj jedną pętlę for.
  */
