6b763e45f117f2fdf6c3127a284700c92c78ea77
KamilWojas <wojaskamil@yahoo.pl>
2024-12-19 23:42:58 +0100

    PPJ_Excercises

--- PPJ_Petle/PetlaFor/ZadanieTablicaRoznice.java
+++ PPJ_Petle/PetlaFor/ZadanieTablicaRoznice.java
@@ -3,3 +3,13 @@ Napisz program, który obliczy różnicę każdego elementu tablicy z sumą wszy
 pozostałych elementów w tablicy. Wynik dla każdego elementu wyświetl na konsoli.
 Wykorzystaj jedną pętlę for.
  */
+
+public class ZadanieTablicaRoznice {
+    public static void main(String[] args) {
+        int[] tab = {3, 5, 7, 9}; // Tablica liczb całkowitych
+
+        // Obliczanie sumy wszystkich elementów tablicy
+        int suma = 0;
+        for (int num : tab) { // Pętla foreach
+            suma += num;
+        }
