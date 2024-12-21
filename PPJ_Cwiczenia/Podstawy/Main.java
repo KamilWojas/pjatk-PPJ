49e8d9aeab2b6d730101d64972f364820d30f115
KamilWojas <wojaskamil@yahoo.pl>
2024-12-21 23:23:02 +0100

    PPJ_Excercises

---
+++ PPJ_Cwiczenia/Podstawy/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1. Deklaracja i inicjalizacja zmiennych
+        int liczbaCalkowita = 10;
+        double liczbaZPrzecinkiem = 3.14;
+        boolean czyPrawda = true;
+        char znak = 'A';
+        String tekst = "Witaj w nauce Javy!";
