7050a70339b0e90fc334b46791df8482508cf073
KamilWojas <wojaskamil@yahoo.pl>
2024-12-21 23:23:45 +0100

    PPJ_Excercises

--- PPJ_Cwiczenia/Podstawy/Main.java
+++ PPJ_Cwiczenia/Podstawy/Main.java
@@ -13,3 +13,14 @@ public class Main {
         System.out.println("Wartość logiczna: " + czyPrawda);
         System.out.println("Znak: " + znak);
         System.out.println("Tekst: " + tekst);
+
+        // 3. Operacje matematyczne
+        int a = 20;
+        int b = 5;
+        System.out.println("Dodawanie: " + (a + b)); // 25
+        System.out.println("Odejmowanie: " + (a - b)); // 15
+        System.out.println("Mnożenie: " + (a * b)); // 100
+        System.out.println("Dzielenie: " + (a / b)); // 4
+        System.out.println("Reszta z dzielenia (modulo): " + (a % b)); // 0
+    }
+}
