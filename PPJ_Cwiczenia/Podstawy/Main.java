daed5bd3220bf368f44cd269621adefd1701d199
KamilWojas <wojaskamil@yahoo.pl>
2024-12-21 23:23:23 +0100

    PPJ_Excercises

--- PPJ_Cwiczenia/Podstawy/Main.java
+++ PPJ_Cwiczenia/Podstawy/Main.java
@@ -6,3 +6,10 @@ public class Main {
         boolean czyPrawda = true;
         char znak = 'A';
         String tekst = "Witaj w nauce Javy!";
+
+        // 2. Wyświetlanie wartości zmiennych
+        System.out.println("Liczba całkowita: " + liczbaCalkowita);
+        System.out.println("Liczba z przecinkiem: " + liczbaZPrzecinkiem);
+        System.out.println("Wartość logiczna: " + czyPrawda);
+        System.out.println("Znak: " + znak);
+        System.out.println("Tekst: " + tekst);
