4708c710570a62113edf2275954df8ea3b3b25b2
KamilWojas <wojaskamil@yahoo.pl>
2024-12-23 00:45:31 +0100

    PPJ_Excercises

--- PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task2.java
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task2.java
@@ -5,5 +5,11 @@ Napisz program, który wypisze tabliczkę mnożenia dla liczby 5.
 public class Task2 {
     public static void main(String[] args){
         
+        int liczba = 5;
+        
+        for (int i = 1; i <= 10; i++){
+            System.out.println(liczba "*" +i+ "=" + (liczba *i));
+        }
+        
     }
 }
