5a865b6763689ec2c2ec11922524736916f0ac3d
KamilWojas <wojaskamil@yahoo.pl>
2024-12-22 22:26:36 +0100

    PPJ_Excercises

---
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/EvenOddCheck.java
@@ -0,0 +1,12 @@
+public class EvenOddCheck {
+    public static void main(String[] args){
+
+        int liczba = 2;
+
+        if (liczba % 2 == 0){
+            System.out.println(liczba + "jest liczbą parzystą.");
+        }else {
+            System.out.println(liczba + "jest liczbą nieparzystą");
+        }
+    }
+}
