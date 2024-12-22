3c08a82419c33db13f382bd3c2f23fcbc0a888a4
KamilWojas <wojaskamil@yahoo.pl>
2024-12-23 00:34:29 +0100

    PPJ_Excercises

--- PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task1.java
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task1.java
@@ -2,3 +2,14 @@
 Napisz program, który wypisze liczby od 1 do 20,
 ale tylko te, które są podzielne przez 3.
  */
+
+public class Task1 {
+    public static void main(String[] args){
+        
+        for (int i = 1; i <= 20; i++){
+            if(i % 3 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
