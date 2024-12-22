f70a19626a0094a0cb17ac619ff9634f2ad067dd
KamilWojas <wojaskamil@yahoo.pl>
2024-12-22 22:55:03 +0100

    PPJ_Excercises

--- PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/LoopExample.java
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/LoopExample.java
@@ -1,5 +1,9 @@
 public class LoopExample {
     public static void main(String[] args){
 
+        for (int i = 1; i <= 10; i++){
+            System.out.println("Liczba" + i);
+        }
+
     }
 }
