e1d3ce0899d9009c1ce435aa365be9a4e9c7a94f
KamilWojas <wojaskamil@yahoo.pl>
2024-12-24 00:14:09 +0100

    PPJ_Excercises

--- PPJ_Cwiczenia/Podstawy/Metody/PrimeNumberCheck.java
+++ PPJ_Cwiczenia/Podstawy/Metody/PrimeNumberCheck.java
@@ -10,3 +10,16 @@ public class PrimeNumberCheck {
             System.out.println(liczba + " nie jest liczbą pierwszą.");
         }
     }
+
+    // Metoda sprawdzająca, czy liczba jest liczbą pierwszą
+    public static boolean czyPierwsza(int n) {
+        if (n <= 1) return false; // Liczby <= 1 nie są pierwsze
+
+        for (int i = 2; i <= Math.sqrt(n); i++) { // Sprawdzamy dzielniki do pierwiastka z n
+            if (n % i == 0) {
+                return false; // Liczba nie jest pierwsza
+            }
+        }
+        return true; // Liczba jest pierwsza
+    }
+}
