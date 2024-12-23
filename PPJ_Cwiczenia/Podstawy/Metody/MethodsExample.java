d1d825b3a2cc037006362bf08cd4970d65463c22
KamilWojas <wojaskamil@yahoo.pl>
2024-12-23 23:58:40 +0100

    PPJ_Excercises

--- PPJ_Cwiczenia/Podstawy/Metody/MethodsExample.java
+++ PPJ_Cwiczenia/Podstawy/Metody/MethodsExample.java
@@ -4,3 +4,9 @@ public class MethodsExample {
         int wynik = kwadrat(liczba); // Wywołanie metody
         System.out.println("Kwadrat liczby " + liczba + " wynosi " + wynik);
     }
+
+    // Metoda obliczająca kwadrat liczby
+    public static int kwadrat(int x) {
+        return x * x; // Zwraca kwadrat liczby
+    }
+}
