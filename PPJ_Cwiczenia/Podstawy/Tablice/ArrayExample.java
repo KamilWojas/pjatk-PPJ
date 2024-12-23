9501d540d25a479ad548d28fed9a9dc4f1192b2b
KamilWojas <wojaskamil@yahoo.pl>
2024-12-23 22:42:07 +0100

    PPJ_Excercises

--- PPJ_Cwiczenia/Podstawy/Tablice/ArrayExample.java
+++ PPJ_Cwiczenia/Podstawy/Tablice/ArrayExample.java
@@ -1,3 +1,11 @@
 public class ArrayExample {
     public static void main(String[] args) {
         int[] liczby = {1, 2, 3, 4, 5}; // Deklaracja i inicjalizacja tablicy
+
+
+        // Wypisywanie wszystkich elementów tablicy
+        for (int i = 0; i < liczby.length; i++) { // Pętla przechodzi przez wszystkie indeksy
+            System.out.println("Element " + i + ": " + liczby[i]);
+        }
+    }
+}
