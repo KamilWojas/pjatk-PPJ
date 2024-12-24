3358227aed11e5c11b5cd759aca7e447fabea177
KamilWojas <wojaskamil@yahoo.pl>
2024-12-24 23:51:53 +0100

    PPJ_Excercises

---
+++ PPJ_Cwiczenia/Podstawy/Metody/EvenNumbersInArray.java
@@ -0,0 +1,13 @@
+public class EvenNumbersInArray {
+    public static void main(String[] args) {
+        int[] liczby = {10, 15, 20, 25, 30}; // Tablica z liczbami
+
+        // Pętla przechodząca przez tablicę
+        for (int i = 0; i < liczby.length; i++) {
+            // Sprawdzamy, czy liczba jest parzysta
+            if (liczby[i] % 2 == 0) {
+                System.out.println(liczby[i]); // Wypisujemy liczbę
+            }
+        }
+    }
+}
