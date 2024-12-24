ae2434d89eca4e477b67457b419af724eaeb7672
KamilWojas <wojaskamil@yahoo.pl>
2024-12-24 23:49:00 +0100

    PPJ_Excercises

---
+++ PPJ_Cwiczenia/Podstawy/Metody/DivisibleBySeven.java
@@ -0,0 +1,6 @@
+public class DivisibleBySeven {
+    public static void main(String[] args) {
+        // Pętla przechodząca przez liczby od 1 do 100
+        for (int i = 1; i <= 100; i++) {
+            // Sprawdzamy, czy liczba jest podzielna przez 7
+            if (i % 7 == 0) {
