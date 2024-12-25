31c8235794746a7f96baa724973671086e4cb0a2
KamilWojas <wojaskamil@yahoo.pl>
2024-12-26 00:02:06 +0100

    Program główny

---
+++ PPJ_Cwiczenia/Podstawy/Dziedziczenie/ZwierzęExample.java
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class ZwierzęExample {
+    public static void main(String[] args) {
+        Pies pies = new Pies();
+        pies.ustawNazwę("Burek");
+        pies.wydajDzwiek();
+    }
+}
