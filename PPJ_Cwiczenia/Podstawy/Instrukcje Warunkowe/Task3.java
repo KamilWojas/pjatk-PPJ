04ac51365dbe76fdf84962fd2f9e418ec9f964c7
KamilWojas <wojaskamil@yahoo.pl>
2024-12-27 23:11:31 +0100

    Pobranie liczby od użytkownika

--- PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task3.java
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task3.java
@@ -6,3 +6,7 @@ Napisz program, który sprawdza, czy podana liczba jest parzysta czy nieparzysta
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        // Pobranie liczby od użytkownika
+        System.out.print("Podaj liczbę: ");
+        int liczba = scanner.nextInt();
