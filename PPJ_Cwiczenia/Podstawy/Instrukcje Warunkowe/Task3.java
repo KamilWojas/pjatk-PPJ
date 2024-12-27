b7c3722341efa9b6e67773434fbe0d124414442e
KamilWojas <wojaskamil@yahoo.pl>
2024-12-27 23:11:58 +0100

    Operator warunkowy

--- PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task3.java
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task3.java
@@ -10,3 +10,6 @@ public class Main {
         // Pobranie liczby od użytkownika
         System.out.print("Podaj liczbę: ");
         int liczba = scanner.nextInt();
+
+        // Operator warunkowy
+        String wynik = (liczba % 2 == 0) ? "Parzysta" : "Nieparzysta";
