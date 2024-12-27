bd267baf9d7ac40303aefb4a07d1edd2e9f42a9e
KamilWojas <wojaskamil@yahoo.pl>
2024-12-27 23:13:40 +0100

    Wynik programu:

--- PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task3.java
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task3.java
@@ -3,7 +3,7 @@ Zadanie 2
 Napisz program, który sprawdza, czy podana liczba jest parzysta czy nieparzysta .
  */
 
-public class Main {
+public class Task3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
