9f55c87956b84e19da47d2c669e3ca36affa4880
KamilWojas <wojaskamil@yahoo.pl>
2024-12-28 11:16:05 +0100

    Pobranie dwóch liczb od użytkownika

--- PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task4.java
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task4.java
@@ -1,3 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+
+        // Pobranie dwóch liczb od użytkownika
+        System.out.print("Podaj pierwszą liczbę: ");
+        int liczba1 = scanner.nextInt();
+        System.out.print("Podaj drugą liczbę: ");
+        int liczba2 = scanner.nextInt();
