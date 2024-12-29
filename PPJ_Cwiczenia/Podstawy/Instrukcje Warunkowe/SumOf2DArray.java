76857d53d3218e7f9627b30f0ea57fd20ac29592
KamilWojas <wojaskamil@yahoo.pl>
2024-12-29 14:09:51 +0100

    Tworzymy tablicę dwuwymiarową

---
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/SumOf2DArray.java
@@ -0,0 +1,7 @@
+public class SumOf2DArray {
+    public static void main(String[] args) {
+        // Tworzymy tablicę dwuwymiarową
+        int[][] tablica = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
