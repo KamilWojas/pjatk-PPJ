b9fd2d3dd3ff1a46f7a14c555699f25be7c7c2d4
KamilWojas <wojaskamil@yahoo.pl>
2024-12-30 23:18:46 +0100

    Tworzymy rozszerzoną tablicę dwuwymiarową

---
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/SumOf2DArrayExtended.java
@@ -0,0 +1,10 @@
+public class SumOf2DArrayExtended {
+    public static void main(String[] args) {
+        // Tworzymy rozszerzoną tablicę dwuwymiarową
+        int[][] tablica = {
+                {1, 2, 3, 4},  // Pierwszy wiersz (4 kolumny)
+                {5, 6, 7, 8},  // Drugi wiersz (4 kolumny)
+                {9, 10, 11, 12} // Trzeci wiersz (4 kolumny)
+        };
+
+        int suma = 0; // Zmienna przechowująca sumę elementów
