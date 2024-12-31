bd0c70a819e9a4afe4c2d3784bfbc1d4ac37ddb8
KamilWojas <wojaskamil@yahoo.pl>
2024-12-31 19:00:18 +0100

    Wypełniam tablicę liczbami od 1 do 10

--- PPJ_Cwiczenia/Podstawy/Tablice wielowymiarowe/TablicaEgzamin.java
+++ PPJ_Cwiczenia/Podstawy/Tablice wielowymiarowe/TablicaEgzamin.java
@@ -1,3 +1,8 @@
 public class TablicaEgzamin {
     public static void main(String[] args) {
         int[] tablica = new int[10]; // Tworzę tablicę o rozmiarze 10
+
+        // Wypełniam tablicę liczbami od 1 do 10
+        for (int i = 0; i < tablica.length; i++) {
+            tablica[i] = i + 1; // Wartości od 1 do 10
+        }
