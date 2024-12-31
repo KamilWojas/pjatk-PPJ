fc8e2f2759a095fec65a6ad733da6ac2b089d2fb
KamilWojas <wojaskamil@yahoo.pl>
2024-12-31 18:55:49 +0100

    Obsługa błędu dzielenia przez zero

--- PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/WyjątkiExample.java
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/WyjątkiExample.java
@@ -5,3 +5,7 @@ public class WyjątkiExample {
             int b = 0; // Próba dzielenia przez zero
             int wynik = a / b;
             System.out.println("Wynik: " + wynik);
+
+        } catch (ArithmeticException e) {
+            // Obsługa błędu dzielenia przez zero
+            System.out.println("Błąd: Nie można dzielić przez zero!");
