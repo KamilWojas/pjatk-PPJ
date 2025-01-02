3c9a5a5140736bd90ae45f5ea8a702aeac9fd207
KamilWojas <wojaskamil@yahoo.pl>
2025-01-02 23:39:00 +0100

    Program główny

---
+++ PPJ_Cwiczenia/Podstawy/Interfejsy/InterfejsExample.java
@@ -0,0 +1,10 @@
+// Program główny
+public class InterfejsExample {
+    public static void main(String[] args) {
+        Dzwiek pies = new Pies();
+        pies.wydajDzwiek();
+
+        Dzwiek kot = new Kot();
+        kot.wydajDzwiek();
+    }
+}
