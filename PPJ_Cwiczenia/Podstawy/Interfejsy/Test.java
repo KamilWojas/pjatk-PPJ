fbfed06588b98b5fac6911282a86c6153621a790
KamilWojas <wojaskamil@yahoo.pl>
2025-01-03 23:25:28 +0100

    Override

--- PPJ_Cwiczenia/Podstawy/Interfejsy/Test.java
+++ PPJ_Cwiczenia/Podstawy/Interfejsy/Test.java
@@ -1,3 +1,13 @@
+
+
+
+
+
+public void wydajDzwiek() {
+        System.out.println("Miau!");
+        }
+}
+
 class Test {
     public static void main(String[] args) {
         Dzwiek kot = new Kot();
