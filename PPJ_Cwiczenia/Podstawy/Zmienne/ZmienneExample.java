89af90bd3f37f74913145188cdb05a4372e30ad6
KamilWojas <wojaskamil@yahoo.pl>
2025-01-04 23:13:12 +0100

    Liczba całkowita

--- PPJ_Cwiczenia/Podstawy/Zmienne/ZmienneExample.java
+++ PPJ_Cwiczenia/Podstawy/Zmienne/ZmienneExample.java
@@ -2,7 +2,7 @@ public class ZmienneExample {
     public static void main(String[] args){
 
         // Deklaracja zmiennych
-        int liczbaCalkowita = 10;
+        int liczbaCalkowita = 10; //Liczba całkowita
 
 
     }
