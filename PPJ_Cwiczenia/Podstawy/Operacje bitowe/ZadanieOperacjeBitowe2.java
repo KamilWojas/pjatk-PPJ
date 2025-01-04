adb7bcbf2d29bbc3ec6497fc720e8f8ee1286894
KamilWojas <wojaskamil@yahoo.pl>
2025-01-05 00:33:22 +0100

    Zadanie2 - Przesunięcie w lewo

--- PPJ_Cwiczenia/Podstawy/Operacje bitowe/ZadanieOperacjeBitowe2.java
+++ PPJ_Cwiczenia/Podstawy/Operacje bitowe/ZadanieOperacjeBitowe2.java
@@ -6,10 +6,10 @@ Zadanie 2: Wykonaj przesunięcie w lewo iw liczbie 8 o 2 bity. Wypisz wyniki.
 public class ZadanieOperacjeBitowe2 {
     public static void main(String[] args){
 
-        int a = 8; //1000
+        int liczba = 8; //1000
 
         //Przesunięcie w lewo
-        int przesunLewo = a << 2; //Wynik : 1000 << 2 =
+        int przesunLewo = liczba  << 2; //Wynik : 1000 << 2 = 100000
         System.out.println("Przesunięcie w lewo :" + przesunLewo);
 
 
