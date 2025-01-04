18897b15770d0b6b12d17ce6ecc16768fb942a8b
KamilWojas <wojaskamil@yahoo.pl>
2025-01-04 23:21:23 +0100

    int

--- PPJ_Cwiczenia/Podstawy/Zmienne/ZmienneExample.java
+++ PPJ_Cwiczenia/Podstawy/Zmienne/ZmienneExample.java
@@ -6,12 +6,15 @@ public class ZmienneExample {
         double liczbaZmiennoPrzecinkowa = 2.12 //Liczba zmiennoprzecinkowa
         char znak = 'A'; //Pojedyńczy znak
         boolean wartoscLogiczna = false //Wartość logiczna
+        String imie = "Kamil";
+        int wiek = 27;
 
         System.out.println("Deklaracja zmiennych");
         System.out.println("Liczba całkowita :" + liczbaCalkowita);
         System.out.println("Liczba zmiennoprzecinkowa :" + liczbaZmiennoPrzecinkowa);
         System.out.println("Pjedyńczy znak :" + znak);
         System.out.println("Wartość logiczna : " + wartoscLogiczna);
+        System.out.println("Imię :" + imie)
 
 
     }
