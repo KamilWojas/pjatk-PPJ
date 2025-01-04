7d02401c862155efc16d8f545751fc1acf6f2be3
KamilWojas <wojaskamil@yahoo.pl>
2025-01-05 00:08:51 +0100

    Zadanie1

--- PPJ_Cwiczenia/Podstawy/Operacje bitowe/ZadanieOpercjeNaBitach1.java
+++ PPJ_Cwiczenia/Podstawy/Operacje bitowe/ZadanieOpercjeNaBitach1.java
@@ -2,3 +2,21 @@
 Zadanie 1: Napisz program, który wykonuje operacje
 AND, OR i XOR na liczbach 12(1100) i 7(0111) oraz wypisuje wyniki.
  */
+
+ public class ZadanieOpercjeNaBitach1 {
+     public static void main(String[] args){
+
+     }
+}
+
+
+
+
+
+/*
+Podstawowe operacje bitowe w Javie:
+AND ( &) – Bit jest podłączony na 1 tylko wtedy, gdy oba bity są 1.
+OR ( |) – Bit jest podłączony na 1, gdy podstawowy jeden z bitów jest 1.
+XOR ( ^) – Bit jest podłączony na 1, gdy dokładnie jeden z bitów jest 1.
+NOT ( ~) – Odwraca wszystkie bity (0 zamienia na 1, a 1 na 0).
+ */
