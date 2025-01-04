5e0322b2cd6007da4ce63bf9efeaeae2d8760b01
KamilWojas <wojaskamil@yahoo.pl>
2025-01-05 00:17:07 +0100

    Zadania

--- PPJ_Cwiczenia/Podstawy/Operacje bitowe/OperacjeBitowe.java
+++ PPJ_Cwiczenia/Podstawy/Operacje bitowe/OperacjeBitowe.java
@@ -35,13 +35,6 @@ public class OperacjeBitowe {
 }
 
 
-
-
-
-
-
-
-
 /*
 Operacje bitowe manipulują bitami (0 i 1) w liczbach binarnych. W Javie liczby ( int, long, short, byte) są dostępne
 w postaci binarnej, więc możemy działać na ich poszczególnych bitach.
@@ -56,4 +49,12 @@ Przesunięcia bitowe:
 >>– Przesunięcie w prawo (dzieli przez 2 dla każdego przesunięcia, następuje znak).
 >>>– Przesunięcie w prawo bez zachowania znaku towarowego (uzupełnia bity zerami).
 
+ */
+
+/*
+Zadanie 1: Napisz program, który wykonuje operacje AND, OR i XOR na liczbach 12(1100) i 7(0111) oraz wypisuje wyniki.
+
+Zadanie 2: Wykonaj przesunięcie w lewo iw liczbie 8o 2 bity. Wypisz wyniki.
+
+Zadanie 3: Zaimplementuj program, który zamienia miejsce z dziesiętnej na binarną i wypisuje jej wartość w formie binarnej.
  */
