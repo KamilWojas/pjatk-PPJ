aa50dfab577c345567d806ffa251fc20645dc101
KamilWojas <wojaskamil@yahoo.pl>
2025-01-06 00:45:05 +0100

    Wyniki

--- PPJ_Cwiczenia/Podstawy/Systemy liczbowe /DodawanieHexOctal.java
+++ PPJ_Cwiczenia/Podstawy/Systemy liczbowe /DodawanieHexOctal.java
@@ -2,8 +2,8 @@ public class DodawanieHexOctal {
     public static void main(String[] args){
 
         //Liczby w różnych systemach
-        String hexLiczba = "1F" //Szesnastkowy
-        String octLiczba = "23" //Ósemkowy 
+        String hexLiczba = "1F"; //Szesnastkowy
+        String octLiczba = "23"; //Ósemkowy 
         
         //Konwersja na dzięstny system
         int dziesietnyHex = Integer.parseInt(hexLiczba, 16); //Hex -> Dziesiętny 
