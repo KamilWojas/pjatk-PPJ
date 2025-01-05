7c00602b9a45a5679aa3d0647cf7fcf90d6930d3
KamilWojas <wojaskamil@yahoo.pl>
2025-01-06 00:34:05 +0100

    Dodawanie liczb binarnych

--- PPJ_Cwiczenia/Podstawy/Systemy liczbowe /SystemyLiczbowe.java
+++ PPJ_Cwiczenia/Podstawy/Systemy liczbowe /SystemyLiczbowe.java
@@ -4,7 +4,14 @@ public class SystemyLiczbowe {
 
         //Konwersja na różne systemy liczbowe
         System.out.println("Liczba dziesiętna : " + liczba);
+        System.out.println("Binarne : " + Integer.toBinaryString(liczba));
+        System.out.println("Ósemkowy : " + Integer.toOctalString(liczba));
+        System.out.println("Szesnastkowy : " + Integer.toHexString(liczba));
 
-
+        // Dodawanie liczb binarnych
+        int liczba1 = Integer.parseInt("1011", 2); // Konwersja z binarnego na dziesiętny
+        int liczba2 = Integer.parseInt("1101", 2); // Konwersja z binarnego na dziesiętny
+        int suma = liczba1 + liczba2;
+        System.out.println("Suma binarna: " + Integer.toBinaryString(suma) + " (dziesiętnie: " + suma + ")");
     }
 }
