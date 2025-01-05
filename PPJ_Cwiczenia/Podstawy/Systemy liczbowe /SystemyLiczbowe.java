abaa380853074cd0f50d9c77da135a800e9bc07d
KamilWojas <wojaskamil@yahoo.pl>
2025-01-05 01:12:11 +0100

    Konwersja na różne systemy liczbowe

--- PPJ_Cwiczenia/Podstawy/Systemy liczbowe /SystemyLiczbowe.java
+++ PPJ_Cwiczenia/Podstawy/Systemy liczbowe /SystemyLiczbowe.java
@@ -1,5 +1,9 @@
 public class SystemyLiczbowe {
     public static void main(String[] args){
+        int liczba = 13;
+
+        //Konwersja na różne systemy liczbowe
+        System.out.println("Liczba dziesiętna : " + liczba);
 
 
     }
