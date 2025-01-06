0a5b0527d360a6f2c311b81e17b4475e3ee734f7
KamilWojas <wojaskamil@yahoo.pl>
2025-01-07 00:22:26 +0100

    Task2_1

--- PPJ_Excercises/Excercises02/Task01.java
+++ PPJ_Excercises/Excercises02/Task01.java
@@ -1,38 +1,21 @@
 public class Task01 {
     public static void main(String[] args) {
-        int value = 14; // Wartość początkowa
 
-        // 1. Przesunięcie o 0 pozycji w prawo
-        int result1 = value >> 0;
-        System.out.println("Przesunięcie o 0 pozycji w prawo: " + result1);
+        int litBin = 165;
+        int litOct = 165;
+        int litHex = 165;
 
-        // 2. Przesunięcie rezultatu o 2 pozycje w lewo
-        int result2 = result1 << 2;
-        System.out.println("Przesunięcie rezultatu o 2 pozycje w lewo: " + result2);
+        System.out.println("Literał w systemie binarnym -> " + litBin + " to 10100101 ");
+        System.out.println("Literał w systemie ósemkowym -> " + litOct + " to 10100101 ");
+        System.out.println("Literał w systemie szesnastkowy -> " + litHex + " to 10100101 ");
 
-        // 3. Przesunięcie rezultatu o 4 pozycje w prawo
-        int result3 = result2 >> 4;
-        System.out.println(
+        litBin = 0b10100101;
+        System.out.println(litBin);
+        litOct = 0245;
+        System.out.println(litOct);
+        litHex = 0xA5;
+        System.out.println(litHex);
 
-                "Przesunięcie rezultatu o 4 pozycje w prawo: " + result3);
+
     }
 }
-
-/*
-Wyniki działania programu:
-Dla wartości 14:
-
-Przesunięcie o 0 pozycji w prawo:
-Wynik: 14 (bez zmian).
-
-Przesunięcie rezultatu o 2 pozycje w lewo:
-Wartość bitowa przesunięta o 2 pozycje w lewo: 14 << 2 = 56.
-
-Przesunięcie rezultatu o 4 pozycje w prawo:
-Wartość bitowa przesunięta o 4 pozycje w prawo: 56 >> 4 = 3.
-
-Wyjście na konsoli:
-Przesunięcie o 0 pozycji w prawo: 14
-Przesunięcie rezultatu o 2 pozycje w lewo: 56
-Przesunięcie rezultatu o 4 pozycje w prawo: 3
- */
