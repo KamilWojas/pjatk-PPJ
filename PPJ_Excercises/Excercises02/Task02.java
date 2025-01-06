d27b9f4f469dc13dcf1fcb64d0008ddc3a4f2d60
KamilWojas <wojaskamil@yahoo.pl>
2025-01-07 00:18:16 +0100

    Iperacje bitowe

--- PPJ_Excercises/Excercises02/Task02.java
+++ PPJ_Excercises/Excercises02/Task02.java
@@ -4,17 +4,18 @@ public class Task02 {
         int value = 14;
         System.out.println("Wartość początkowa: " + value);
 
-        // Przesunięcie o 0 pozycji w prawo
-        int shiftRight0 = value >> 0;
-        System.out.println("Po przesunięciu o 0 pozycji w prawo: " + shiftRight0);
+        int wPrawo = value >> 0;
+        System.out.println("Przesunięcie o 0 w prawo: " + wPrawo);
+        int wLewo = wPrawo << 2;
+        System.out.println("Przesunięcie o 2 w lewo: " + wLewo);
 
-        // Przesunięcie rezultatu o 2 pozycje w lewo
-        int shiftLeft2 = shiftRight0 << 2;
-        System.out.println("Po przesunięciu o 2 pozycje w lewo: " + shiftLeft2);
+        int wLewoo = wLewo << 2;
+        System.out.println("Przesinięcie o 2 w lewo: " + wLewoo);
 
-        // Przesunięcie rezultatu o 4 pozycje w prawo
-        int shiftRight4 = shiftLeft2 >> 4;
-        System.out.println("Po przesunięciu o 4 pozycje w prawo: " + shiftRight4);
+        int wLewooo = 56 >> 4;
+        System.out.println("W prawo o 4: " + wLewooo);
+        int wLewoooo = 0b111000 >> 4;
+        System.out.println("W prawo o 4 bin: " + wLewoooo);
     }
 }
 
