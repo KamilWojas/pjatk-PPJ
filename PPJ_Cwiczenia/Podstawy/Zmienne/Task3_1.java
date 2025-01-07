6cdaccbcdd28fbcf74b2ed12de38cb4f96709386
KamilWojas <wojaskamil@yahoo.pl>
2025-01-07 23:01:06 +0100

    Zmienne

---
+++ PPJ_Cwiczenia/Podstawy/Zmienne/Task3_1.java
@@ -0,0 +1,10 @@
+public class Task3_1 {
+    
+    public static void main(String[] args){
+        
+        byte najmniejszaWartosc = -128;
+        byte najwiekszaWartosc = 127;
+        System.out.println("Najmniejsza wartość -> " + najmniejszaWartosc + "\n" + "Największa wartość -> " 
+                + najwiekszaWartosc);
+    }
+}
