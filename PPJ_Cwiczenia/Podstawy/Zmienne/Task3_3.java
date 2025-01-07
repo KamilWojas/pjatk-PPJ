4aa30a160da31c16e192339b4df2ead61b1764d9
KamilWojas <wojaskamil@yahoo.pl>
2025-01-07 23:59:53 +0100

    Zmienne

--- PPJ_Cwiczenia/Podstawy/Zmienne/Task3_3.java
+++ PPJ_Cwiczenia/Podstawy/Zmienne/Task3_3.java
@@ -1,5 +1,11 @@
 public class Task3_3 {
     public static void main(String[] args){
-        
+
+        int a11 = 10;
+        int 1abc = 9;
+        int #abc = 3;
+        int void = 7;
+        int null = 0;
+
     }
 }
