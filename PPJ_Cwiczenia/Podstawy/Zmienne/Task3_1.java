5a37be7c84a053cf6aabb9d656e2f3c46e5b9d37
KamilWojas <wojaskamil@yahoo.pl>
2025-01-07 23:07:36 +0100

    Zmienne

--- PPJ_Cwiczenia/Podstawy/Zmienne/Task3_1.java
+++ PPJ_Cwiczenia/Podstawy/Zmienne/Task3_1.java
@@ -4,12 +4,12 @@ public class Task3_1 {
         
         byte najmniejszaWartosc = -128;
         byte najwiekszaWartosc = 127;
-        System.out.println("Najmniejsza wartość -> " + najmniejszaWartosc + "\n" + "Największa wartość -> " 
+        System.out.println("Najmniejsza wartość byte -> " + najmniejszaWartosc + "\n" + "Największa wartość byte -> " 
                 + najwiekszaWartosc);
 
         short min = -32768;
         short max = 32767;
-        System.out.println("Najmniejsza wartość -> " + min + "\n" + "Największa wartość -> "
+        System.out.println("Najmniejsza wartość short -> " + min + "\n" + "Największa wartość short -> "
                 + max);
         
     }
