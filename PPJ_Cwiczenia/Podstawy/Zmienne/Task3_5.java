9bbe83442079f71461b371851d5cdd6103d7a0cf
KamilWojas <wojaskamil@yahoo.pl>
2025-01-08 00:09:52 +0100

    Zmienne

---
+++ PPJ_Cwiczenia/Podstawy/Zmienne/Task3_5.java
@@ -0,0 +1,9 @@
+public class Task3_5 {
+    public static void main(String[] args){
+
+        char charValue = 'a';
+        char charvalue = 'b';
+        System.out.println(charValue);
+        System.out.println(charvalue);
+    }
+}
