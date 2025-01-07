bfa802a8ac525e421978c18da196997ccb26d51e
KamilWojas <wojaskamil@yahoo.pl>
2025-01-08 00:23:15 +0100

    Zmienne

---
+++ PPJ_Cwiczenia/Podstawy/Zmienne/Task3_6.java
@@ -0,0 +1,13 @@
+public class Task3_6 {
+    public static void main(String[] args){
+        
+        int a = 5;
+        double b = 3.14;
+        
+        a = (int) b;
+        b = a;
+
+        System.out.println(a)
+        System.out.println(b);;
+    }
+}
