9673f79a2af479cc69aeff4329edb02c68115cca
KamilWojas <wojaskamil@yahoo.pl>
2025-01-08 00:05:04 +0100

    Zmienne

---
+++ PPJ_Cwiczenia/Podstawy/Zmienne/Task3_4.java
@@ -0,0 +1,9 @@
+public class Task3_4 {
+    public static void main(String[] args){
+        
+        int zmienna = 10;
+
+        System.out.println("Zmienna to " + zmienna);
+        System.out.println(zmienna);
+    }
+}
