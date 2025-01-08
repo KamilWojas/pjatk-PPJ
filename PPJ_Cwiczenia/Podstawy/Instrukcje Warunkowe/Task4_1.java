8176ab47d98ffed300d794a82e002635f4a13fce
KamilWojas <wojaskamil@yahoo.pl>
2025-01-09 00:59:48 +0100

    Task4_1

--- PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task4_1.java
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task4_1.java
@@ -2,7 +2,7 @@
 //Jaki da wynik
 
 int i = 10;
-        i = i++;
-        i = ++i;
-        i = i++ + ++i;
-        System.out.println(i);
+        i = i++; //da 10
+        i = ++i; //da 11
+        i = i++ + ++i; //da 11 -> 12 i zwikeszane jest o 1 czyli 13
+        System.out.println(i); //11 +13 = 24
