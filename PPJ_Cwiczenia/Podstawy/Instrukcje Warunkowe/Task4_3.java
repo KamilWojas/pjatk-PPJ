a962389530a3dad734d520c80b317372fd1d9968
KamilWojas <wojaskamil@yahoo.pl>
2025-01-09 01:28:59 +0100

    Task4_3

---
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task4_3.java
@@ -0,0 +1,15 @@
+public class Task4_3 {
+    public static void main(String[] args) {
+
+
+
+        int a = 1;
+        int b = 3;
+        int c = 6;
+        int najwieksza;
+
+        najwieksza = a >= b ? b : c;
+        System.out.println(najwieksza);
+
+    }
+}
