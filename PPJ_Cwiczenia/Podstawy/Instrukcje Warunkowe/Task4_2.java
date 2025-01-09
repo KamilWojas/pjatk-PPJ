b2b380a16e6b10a91f94aaff5d60aaa446d69f08
KamilWojas <wojaskamil@yahoo.pl>
2025-01-09 01:11:10 +0100

    Task4_1

---
+++ PPJ_Cwiczenia/Podstawy/Instrukcje Warunkowe/Task4_2.java
@@ -0,0 +1,15 @@
+//Analiza kodu
+
+
+public class Task4_2 {
+    public static void main(String[] args) {
+
+        double a = 1;
+        double x;
+
+        if (a >= 0) x= 1.701;
+        if (a < 0) x = 2D*3.14f;
+        System.out.println(x); //wartość x nie jest zainicjalizowana więc nie skompiluje się program 
+
+    }
+}
