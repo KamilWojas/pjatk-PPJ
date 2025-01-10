dd67bd4a4109c1c4dd295768994b1a8a2168cc42
KamilWojas <wojaskamil@yahoo.pl>
2025-01-10 22:34:07 +0100

    Void

--- PPJ_Cwiczenia/Podstawy/Podstawy podstaw/Student.java
+++ PPJ_Cwiczenia/Podstawy/Podstawy podstaw/Student.java
@@ -2,3 +2,11 @@ public class Student {
     String imie;
     String nazwisko;
     int rokUrodzenia;
+
+    public void obliczwiek(){
+        System.out.println(2024 - rokUrodzenia);
+    }
+
+    public int zwrocWiek(){
+        return 2024 - rokUrodzenia;
+    }
