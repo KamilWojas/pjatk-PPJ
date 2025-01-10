02122f0d3ff71305c8efc385d451a14332ce99b6
KamilWojas <wojaskamil@yahoo.pl>
2025-01-10 23:58:47 +0100

    Override

--- PPJ_Cwiczenia/Podstawy/Podstawy podstaw/Osoba.java
+++ PPJ_Cwiczenia/Podstawy/Podstawy podstaw/Osoba.java
@@ -6,25 +6,30 @@ public class Osoba {
     private String nazwisko;
     private int rokUrodzenia;
     private int rokSmierci;
+    private int nrPesel;
 
     public Osoba (String imieKonkretnejOsoby
             , String nazwiskokonkretnejOsoby
-            , int rokUrodzeniaKonkretnejOsoby)
+            , int rokUrodzeniaKonkretnejOsoby, int nrPeselKonkretnejOsby)
     {
         this.imie = imieKonkretnejOsoby;
         this.nazwisko = nazwiskokonkretnejOsoby;
         this.rokUrodzenia = rokUrodzeniaKonkretnejOsoby;
+        this.nrPesel = nrPeselKonkretnejOsby;
         liczbaOsobWsystemie++;
     }
 
     public Osoba( String imieKonkretnejOsoby
             ,String nazwiskoKonkrentejOosby
             ,int rokUrodzeniaKonkrentejOsoby,
-                  int rokSmierciKonkretnejOsoby){
+                  int rokSmierciKonkretnejOsoby,
+                  int nrPeselKonkretnejOsoby){
+
         this.imie = imieKonkretnejOsoby;
         this.nazwisko = nazwiskoKonkrentejOosby;
         this.rokUrodzenia = rokUrodzeniaKonkrentejOsoby;
         this.rokSmierci = rokSmierciKonkretnejOsoby;
+        this.nrPesel = nrPeselKonkretnejOsoby;
         liczbaOsobWsystemie++;
     }
     
