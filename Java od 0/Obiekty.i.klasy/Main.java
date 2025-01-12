71714fa4e29759f2d1fbd451eec7d2cc9b8610de
KamilWojas <wojaskamil@yahoo.pl>
2025-01-12 23:08:45 +0100

    metoda

--- Java od 0/Obiekty.i.klasy/Main.java
+++ Java od 0/Obiekty.i.klasy/Main.java
@@ -1,18 +1,32 @@
 public class Main {
     public static void main(String[] args) {
         objectAndClass();
+        objectClass();
+        
 
     }
 
-      /*  Dog ares = new Dog();
-        Dog tajson = new Dog();
-        Dog toska = new Dog();
+    private static void objectClass() {
+        String text = " ";
+        int amount = 8;
+        Boolean bool = false;
+        Dog dog = new Dog();
 
-        ares.name = "Ares";
-        ares.numberOfLegs = 4;
-        ares.hasTail = true;
+        text.equals("");
+        text.toString();
 
-       */
+        bool.equals(true);
+    }
+
+    /*  Dog ares = new Dog();
+      Dog tajson = new Dog();
+      Dog toska = new Dog();
+
+      ares.name = "Ares";
+      ares.numberOfLegs = 4;
+      ares.hasTail = true;
+
+     */
       public static void objectAndClass(){
 
           Dog piesPierwszy = new Dog("Ares", 4, true);
