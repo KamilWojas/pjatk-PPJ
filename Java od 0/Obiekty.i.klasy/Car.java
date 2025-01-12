2c687c9a0c4129b0d9245adb40cf1239460d885b
KamilWojas <wojaskamil@yahoo.pl>
2025-01-13 00:02:56 +0100

    Konstruktor car

---
+++ Java od 0/Obiekty.i.klasy/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+    
+    public Car(){
+        rocznik = 2025;
+        przebieg = 0;
+        
+        
+        
+    }
+    
+    String model;
+    int rocznik;
+    int przebieg;
+}
