1771484b0296bf88680526cbbe843d17cb0d7ae3
KamilWojas <wojaskamil@yahoo.pl>
2025-01-12 18:00:51 +0100

    Override

--- Java od 0/Obiekty.i.klasy/Dog.java
+++ Java od 0/Obiekty.i.klasy/Dog.java
@@ -1,8 +1,16 @@
 public class Dog {
 
-    String name;
-    int numberOfLegs;
-    boolean hasTail;
+   private String name = "Ares";
+   private int numberOfLegs = 4;
+   private boolean hasTail = true;
+   
+   public Dog (String imieKonkretnegoPsa){
+       this.name = imieKonkretnegoPsa;
+   }
+   
+   public String getName(){
+       return getName();
+   }
     
     
 }
--- Java od 0/Obiekty.i.klasy/Main.java
+++ Java od 0/Obiekty.i.klasy/Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        Dog ares = new Dog();
+      /*  Dog ares = new Dog();
         Dog tajson = new Dog();
         Dog toska = new Dog();
 
         ares.name = "Ares";
         ares.numberOfLegs = 4;
         ares.hasTail = true;
+
+       */
+
+        Dog piesPierwszy = new Dog("Ares");
+        
+
+
     }
 }
