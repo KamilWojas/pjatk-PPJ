f0a363f21842a6ebce472fb16408b458bf27ed2f
KamilWojas <wojaskamil@yahoo.pl>
2025-01-13 23:48:27 +0100

    for

--- Java od 0/Petle/Main.java
+++ Java od 0/Petle/Main.java
@@ -5,5 +5,19 @@ public class Main {
         for (int i = 0; i < 7; i++){
             System.out.println("Cześć!");
         }
+
+        Long x = 10L;
+        for (; x <= 12; x++){
+            System.out.println("Cześć 2");
+
+        }
+
+        System.out.println("=======================" + "\n");
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        for (int i = 0; i < arr.length/2; i++)
+            System.out.println(
+                    arr[i % 3 == 0 ? arr.length-1-i : i]
+            );
+
     }
 }
