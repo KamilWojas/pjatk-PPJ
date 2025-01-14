8e703c91813aa9f1a1e912a9062c4cd834d13af7
KamilWojas <wojaskamil@yahoo.pl>
2025-01-14 23:34:59 +0100

    pętla fori

--- Java od 0/Petle/Main.java
+++ Java od 0/Petle/Main.java
@@ -9,7 +9,7 @@ public class Main {
         whileAndDoWhile();
 
         System.out.println("Kolejka klientów");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             if (i == 7 || i == 13){
                 System.out.println("Pomiń klienta");
                 continue;
