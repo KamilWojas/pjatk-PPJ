cf436526eacfa3ae8056bf28a14683f659eba50e
KamilWojas <wojaskamil@yahoo.pl>
2025-01-16 23:08:59 +0100

    kwadrat 10x10

--- Java od 0/PetleZagniezdzone/Main.java
+++ Java od 0/PetleZagniezdzone/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        //kwadrat 10x10
         for (int i = 0; i < 10; i++) {
+            StringBuilder line = new Stringbuilder("");
+            for (int j = 0; j < 10; j++) {
+                line.append("*")
+
+            }
+            System.out.println(line);
 
         }
     }
