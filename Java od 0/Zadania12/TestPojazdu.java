48e0f1f362d275583997e91e4dca2f2b7cd5737b
KamilWojas <wojaskamil@yahoo.pl>
2025-01-19 22:58:16 +0100

    klasa główna

--- Java od 0/Zadania12/TestPojazdu.java
+++ Java od 0/Zadania12/TestPojazdu.java
@@ -1,2 +1,12 @@
 public class TestPojazdu{
     public static void main(String[] args) {
+
+        try{
+            CiagnikSidlowy ciagnik = new CiagnikSidlowy("Black", 4, 10);
+            ciagnik.rozpoczniejJazde();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
+    
