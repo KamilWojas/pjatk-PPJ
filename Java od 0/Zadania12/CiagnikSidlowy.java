9a6811cc7d3d6d724fac002ba5e761d2f9bc722b
KamilWojas <wojaskamil@yahoo.pl>
2025-01-19 22:57:13 +0100

    metoda rozpoczniejJazde

--- Java od 0/Zadania12/CiagnikSidlowy.java
+++ Java od 0/Zadania12/CiagnikSidlowy.java
@@ -6,3 +6,17 @@ class CiagnikSidlowy extends PojazdKolowy{
         super(color, iloscOsi);
         this.masa = masa;
     }
+
+    public void rozpoczniejJazde() throws Exception {
+        double naicskNaOs = masa / getIloscOsi();
+        System.out.println("Rozpoczęcie procedury sprawdzenia jaki jest nacisk pojazdu na oś" + "\n");
+        System.out.println("Nacik na oś: " + naicskNaOs + " ton");
+        if (naicskNaOs < 11) {
+            throw new Exception("Jazda niebiezpieczna, odmowa uruchomienia silnika");
+        }
+
+        System.out.println("Nacisk na oś: " + naicskNaOs + "ton" + "Silnik uruchomiony");
+    }
+
+
+}
