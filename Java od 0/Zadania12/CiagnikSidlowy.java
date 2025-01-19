f3976975f79ae0931a3b3ad095f69e469431c6fb
KamilWojas <wojaskamil@yahoo.pl>
2025-01-19 22:56:55 +0100

    clasa CiagnikSidlowy

---
+++ Java od 0/Zadania12/CiagnikSidlowy.java
@@ -0,0 +1,8 @@
+class CiagnikSidlowy extends PojazdKolowy{
+
+    private double masa;
+
+    public CiagnikSidlowy(String color, int iloscOsi, double masa){
+        super(color, iloscOsi);
+        this.masa = masa;
+    }
