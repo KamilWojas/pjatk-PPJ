1230f60283880e465e9effea6583ff0decd0d2ed
KamilWojas <wojaskamil@yahoo.pl>
2025-01-20 23:11:54 +0100

    konstruktor

--- Java od 0/Dziedziczenie/Cat.java
+++ Java od 0/Dziedziczenie/Cat.java
@@ -3,3 +3,8 @@ public class Cat extends Mammal{
     String color;
     boolean isIndoor;
     String name;
+
+    public Cat(String colorOfCat, boolean isCatIndoor){
+        this.color = colorOfCat;
+        this.isIndoor = isCatIndoor;
+    }
