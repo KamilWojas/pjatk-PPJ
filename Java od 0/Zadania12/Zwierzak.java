d00d07622395451d9e964bd96d06c2f543fdf287
KamilWojas <wojaskamil@yahoo.pl>
2025-01-20 23:05:57 +0100

    Konstruktor

--- Java od 0/Zadania12/Zwierzak.java
+++ Java od 0/Zadania12/Zwierzak.java
@@ -3,3 +3,8 @@ public class Zwierzak {
     private String name;
     private int wiek;
     private int waga;
+
+    public Zwierzak(String imie, int wiek) {
+        this.name = imie;
+        this.wiek = wiek;
+    }
