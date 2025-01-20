63c5aeaa21ab7a37cc3473e0372685ae413ee56d
KamilWojas <wojaskamil@yahoo.pl>
2025-01-20 23:10:25 +0100

    Bird

--- Java od 0/Dziedziczenie/Bird.java
+++ Java od 0/Dziedziczenie/Bird.java
@@ -1,6 +1,21 @@
  class Bird extends Animal{
 
-    void type(){
+     String species;
+     boolean canFly;
 
-    }
-}
+     public Bird(String speciesOfBird, boolean birdCanFly) {
+         this.species = speciesOfBird;
+         this.canFly = birdCanFly;
+     }
+
+     void fly(){
+         if (canFly){
+             System.out.println(species + " Potrafi latac");
+         }else {
+             System.out.println(species + "Nie potrafi latać");
+         }
+     }
+     void chrip(){
+         System.out.println("Ćwir ćwir");
+     }
+ }
