73e4b279b47ce19babc53bcce6f121effd47ea2c
KamilWojas <wojaskamil@yahoo.pl>
2025-01-20 23:13:26 +0100

    metoda isCatIndoor

--- Java od 0/Dziedziczenie/Cat.java
+++ Java od 0/Dziedziczenie/Cat.java
@@ -16,3 +16,12 @@ public class Cat extends Mammal{
     void hunt(){
         System.out.println("cat is hunting a mouse");
     }
+
+    void isCatIndoor(){
+        if (isIndoor){
+            System.out.println(isIndoor + "Cat is home");
+        }else {
+            System.out.println(isIndoor + "cat is not at home ");
+        }
+    }
+}
