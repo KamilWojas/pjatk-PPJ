c4d824292c52c6aa920e579aa7e050864891f04b
KamilWojas <wojaskamil@yahoo.pl>
2025-01-21 23:49:46 +0100

    klasa główna

---
+++ Java od 0/Zadania12/Osoba.java
@@ -0,0 +1,9 @@
+public class Osoba {
+
+    static int liczbaOsobWsystemie;
+
+    private String imie;
+    private String nazwisko;
+    private int rokUrodzenia;
+    private int rokSmierci;
+    private float nrPesel;
