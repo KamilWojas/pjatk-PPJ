c2dad2d578dca81cf58d7d40635d9f6b842ab188
KamilWojas <wojaskamil@yahoo.pl>
2025-01-21 23:49:14 +0100

    klasa główna

---
+++ Java od 0/Zadania12/Nauczyciel.java
@@ -0,0 +1,9 @@
+public class Nauczyciel extends Osoba{
+
+    public Nauczyciel(String imieKonkretnejOsoby, String nazwiskokonkretnejOsoby, int rokUrodzeniaKonkretnejOsoby, float nrPeselKonkretnejOsby) {
+        super(imieKonkretnejOsoby, nazwiskokonkretnejOsoby, rokUrodzeniaKonkretnejOsoby, nrPeselKonkretnejOsby);
+    }
+
+    String przedmiot;
+
+}
