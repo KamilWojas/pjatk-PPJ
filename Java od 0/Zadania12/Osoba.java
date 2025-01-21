eca0e6fd3d595432194158316779c97422aa7b40
KamilWojas <wojaskamil@yahoo.pl>
2025-01-21 23:50:05 +0100

    konstruktor

--- Java od 0/Zadania12/Osoba.java
+++ Java od 0/Zadania12/Osoba.java
@@ -7,3 +7,14 @@ public class Osoba {
     private int rokUrodzenia;
     private int rokSmierci;
     private float nrPesel;
+
+    public Osoba(String imieKonkretnejOsoby
+            , String nazwiskokonkretnejOsoby
+            , int rokUrodzeniaKonkretnejOsoby,
+                 float nrPeselKonkretnejOsby) {
+        this.imie = imieKonkretnejOsoby;
+        this.nazwisko = nazwiskokonkretnejOsoby;
+        this.rokUrodzenia = rokUrodzeniaKonkretnejOsoby;
+        this.nrPesel = nrPeselKonkretnejOsby;
+        liczbaOsobWsystemie++;
+    }
