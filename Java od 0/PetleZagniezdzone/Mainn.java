760ee020ce589b6d6f17cf3d06ab6e27b60965e3
KamilWojas <wojaskamil@yahoo.pl>
2025-01-23 22:34:49 +0100

    zagnieższony for

--- Java od 0/PetleZagniezdzone/Mainn.java
+++ Java od 0/PetleZagniezdzone/Mainn.java
@@ -7,4 +7,6 @@ public class Mainn {
 
         for (int lenght = 0; lenght < 20 ; lenght++) {
             StringBuilder line = new StringBuilder("");
+            for (int width = 1; width < lenght; width++) {
+                line.append("( " + lenght + ", " + width + ");");
     }
