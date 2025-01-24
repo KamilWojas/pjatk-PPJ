156e841758b0c36ca2f97a2635e769e39510576b
KamilWojas <wojaskamil@yahoo.pl>
2025-01-24 23:26:36 +0100

    Drukowanie

--- Java od 0/Petle/Petla.java
+++ Java od 0/Petle/Petla.java
@@ -4,7 +4,9 @@ public class Petla {
         int[] tab = {100, 90, 80, 70}; // Tablica liczb całkowitych
 
         for (int i = 0; i < tab.length; i++) {
-            for (int j = i + 1; j < tab.length; j++) { 
-            
+            for (int j = i + 1; j < tab.length; j++) {
+                System.out.println("Różnica między tab[" + i + "] i tab[" + j + "]: " + (tab[i] - tab[j]));
 
-    }
+
+
+            }
