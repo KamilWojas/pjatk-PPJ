f6434451404e16b8cd281e697751fbf4a8da5d20
KamilWojas <wojaskamil@yahoo.pl>
2025-01-24 23:22:12 +0100

    Pętla

--- Java od 0/PetleZagniezdzone/Mainn.java
+++ Java od 0/PetleZagniezdzone/Mainn.java
@@ -50,6 +50,9 @@ public class Mainn {
     
     private static void drawingRectangle() {
         for (int i = 0; i < 10; i++) {
+            StringBuilder line = new StringBuilder("");
+
+        }
 
         }
 
