20d9468012e45d11c3aee7dbf25150be3cc54239
KamilWojas <wojaskamil@yahoo.pl>
2025-01-26 00:52:18 +0100

    Deklaracja tablicy

--- Java od 0/Tablice/Task4.java
+++ Java od 0/Tablice/Task4.java
@@ -2,7 +2,9 @@ public class Task4 {
 
     public static void main(String[] args) {
         throws IOException{
-            
+
+            int[] arr = {0b11111111, 0401, 514, 0x203};
+
         }
     }
 }
