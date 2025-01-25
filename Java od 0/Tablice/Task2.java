f342bfb39e905eec9d0ce32d475139734e91355d
KamilWojas <wojaskamil@yahoo.pl>
2025-01-25 22:52:45 +0100

    Deklaracja talicy

--- Java od 0/Tablice/Task2.java
+++ Java od 0/Tablice/Task2.java
@@ -5,7 +5,9 @@ public class Task2 {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
         for (int i = 0; i < arr.length / 2; i++) {
-
+            System.out.print(
+                    arr[i % 3 == 0 ? arr.length - 1 - i : i]
+        };
     }
 }
 
