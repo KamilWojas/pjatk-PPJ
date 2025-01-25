ab3fdc169ac64d157a61fdf8129e982791dd2b26
KamilWojas <wojaskamil@yahoo.pl>
2025-01-26 00:54:33 +0100

    Task4

--- Java od 0/Tablice/Task4.java
+++ Java od 0/Tablice/Task4.java
@@ -6,7 +6,11 @@ public class Task4 {
             int[] arr = {0b11111111, 0401, 514, 0x203};
 
             FileOutputSream fos = new FileOutputStream("out.bin");
+            for (int i = args.length - 1; i >=0; i--){
+             fos.write(arr[i]);
+            }
 
+            fos.close();
         }
     }
 }
