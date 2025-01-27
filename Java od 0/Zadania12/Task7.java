6ef96eba15ca4158568bcbec9a04c46ef66dfd89
KamilWojas <wojaskamil@yahoo.pl>
2025-01-28 00:08:03 +0100

    Deklaracja tablicy

--- Java od 0/Zadania12/Task7.java
+++ Java od 0/Zadania12/Task7.java
@@ -1,6 +1,13 @@
 public class Task7 {
     public static void main(String[] args) {
 
+
+        int tab[][] = {
+                    { 1, 2, 3, 4 },
+                    { 5, 6, 7, 8 },
+                    { 9, 10, 11, 12 },
+                    { 13, 14, 15, 16 }
+            };
         
     }
 }
