51896ce3250fcf15294bebaa4c5e82a6ce488db3
KamilWojas <wojaskamil@yahoo.pl>
2025-01-29 23:09:12 +0100

    Task9

---
+++ Java od 0/Zadania12/Task9.java
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Main {
+    public static void main(String[] args) {
+        A objA = new A(10);
+        B objB = new B(20, 5.5);
+
+        objA.show(); 
+        objB.show();
+    }
+}
