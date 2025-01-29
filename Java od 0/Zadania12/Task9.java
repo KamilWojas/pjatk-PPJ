9241b5547fe36fbabddf2eff3cb98090ae5249e0
KamilWojas <wojaskamil@yahoo.pl>
2025-01-29 23:14:20 +0100

    objA.show() wypisze "Klasa A, wartość: 10". objB.show() wypisze "Klasa B, wartość: 5.5", ponieważ metoda show() w B przesłania tę z A.

--- Java od 0/Zadania12/Task9.java
+++ Java od 0/Zadania12/Task9.java
@@ -26,10 +26,11 @@ class B extends A {
 
 class Main {
     public static void main(String[] args) {
+        
         A objA = new A(10);
         B objB = new B(20, 5.5);
 
-        objA.show(); 
+        objA.show();
         objB.show();
     }
 }
