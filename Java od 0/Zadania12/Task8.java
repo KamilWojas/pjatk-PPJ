bd0d9b6d7d5366318e7fd4fd68dc8d89e50eba79
KamilWojas <wojaskamil@yahoo.pl>
2025-01-29 20:43:04 +0100

    Task8

---
+++ Java od 0/Zadania12/Task8.java
@@ -0,0 +1,17 @@
+class Skrzynia{
+
+    private boolean pelna;
+
+    public Skrzynia(){
+        this.pelna = false;
+        validate();
+    }
+
+    private void validate(){
+        if (!pelna){
+            throw new PustaSkrzyniaException("Skrzynia jest pusta! pech!");
+        }
+    }
+}
+
+ 
