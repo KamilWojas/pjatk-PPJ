1c8c593ba0be7833f685ad5f45e50a2749b9489f
KamilWojas <wojaskamil@yahoo.pl>
2025-02-02 22:56:18 +0100

    Konstruktor rakieta

--- Java od 0/Zadania12/Task13.java
+++ Java od 0/Zadania12/Task13.java
@@ -2,4 +2,9 @@ public class Rakieta {
 
     private String nazwa;
     private int wagaPaliwa;
+
+    public Rakieta(String nazwa, int wagaPaliwa){
+        this.nazwa = nazwa;
+        this.wagaPaliwa = wagaPaliwa;
+    }
 }
