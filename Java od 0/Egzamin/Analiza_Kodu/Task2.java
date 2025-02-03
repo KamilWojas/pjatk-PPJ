b87bef7ccac820db38cfe883749609927f0cef8b
KamilWojas <wojaskamil@yahoo.pl>
2025-02-03 23:30:44 +0100

    Odczyt pliku

--- Java od 0/Egzamin/Analiza_Kodu/Task2.java
+++ Java od 0/Egzamin/Analiza_Kodu/Task2.java
@@ -2,4 +2,7 @@ class Task2 {
     public static void main(String[] args) throws IOException {
         int[] data = { 0b11001100, 0123, 0x7A, 200 };
 
+        FileOutputStream fos = new FileOutputStream("example2.bin");
+
+
     }
