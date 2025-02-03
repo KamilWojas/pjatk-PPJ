29dc07cb44b32ce32d9d236965e77e091d399472
KamilWojas <wojaskamil@yahoo.pl>
2025-02-03 23:22:30 +0100

    Inicjacja pętli for

--- Java od 0/Egzamin/Task1.java
+++ Java od 0/Egzamin/Task1.java
@@ -4,6 +4,8 @@ class Task1 {
         int[] numbers = { 0b10101010, 075, 0x3F, 255 };
 
         FileOutputStream fos = new FileOutputStream("example1.bin");
-
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            fos.write(numbers[i]);
+        }
 
     }
