548f812fa0968bde2777c038fda6b29c1489aa8e
KamilWojas <wojaskamil@yahoo.pl>
2025-02-04 23:53:43 +0100

    Task6

--- Java od 0/Egzamin/Analiza_Kodu/Task6.java
+++ Java od 0/Egzamin/Analiza_Kodu/Task6.java
@@ -10,3 +10,11 @@ public class Task6 {
         System.out.print(b + " " + a);
     }
 }
+
+//    Możliwe odpowiedzi:
+//        a) 6 2 9 8
+//        b) 6 3 9 9
+//        c) 6 3 9 8
+//        d) 6 3 9 10
+//        e) 7 3 9 8
+
