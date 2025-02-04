9297e795edd57bd97a1726a1a74f7c176a0e69ec
KamilWojas <wojaskamil@yahoo.pl>
2025-02-04 23:41:21 +0100

    Task5

--- Java od 0/Egzamin/Analiza_Kodu/Task5.java
+++ Java od 0/Egzamin/Analiza_Kodu/Task5.java
@@ -1,2 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int x = 8;
+        {
+            int y = 4;
+            System.out.print(++y + " " + x + " ");
+            x = y * 2;
+        }
+    }
