3b4f937e8be31aef884efde0c19ef1262c8c9c56
KamilWojas <wojaskamil@yahoo.pl>
2025-02-04 23:52:50 +0100

    Task6

--- Java od 0/Egzamin/Analiza_Kodu/Task6.java
+++ Java od 0/Egzamin/Analiza_Kodu/Task6.java
@@ -1,2 +1,9 @@
 public class Task6 {
     public static void main(String[] args) {
+        int a = 2;
+        {
+            int b = 6;
+            System.out.print(b + " " + (++a) + " ");
+            a += b;
+        }
+    }
