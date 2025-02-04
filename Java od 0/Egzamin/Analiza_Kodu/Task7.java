0fd57bde5ecf9195a13d368670a97898089bd931
KamilWojas <wojaskamil@yahoo.pl>
2025-02-04 23:56:31 +0100

    Task7

--- Java od 0/Egzamin/Analiza_Kodu/Task7.java
+++ Java od 0/Egzamin/Analiza_Kodu/Task7.java
@@ -1,2 +1,9 @@
 public class Task7 {
     public static void main(String[] args) {
+        int x = 4;
+        {
+            int y = 9;
+            System.out.print(y + " " + (x++) + " ");
+            x += y;
+        }
+    }
