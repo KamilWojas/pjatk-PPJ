5ed57fa475f2b8f0944d429e06a79420181036fd
KamilWojas <wojaskamil@yahoo.pl>
2025-02-04 23:18:58 +0100

    Task3

--- Java od 0/Egzamin/Analiza_Kodu/Task3.java
+++ Java od 0/Egzamin/Analiza_Kodu/Task3.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         int x = 5;
-
+        {
+            int y = 10;
+            System.out.print(y++ + " " + x + " ");
+            x += y;
+        }
     }
