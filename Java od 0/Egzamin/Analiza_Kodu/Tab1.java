1021110994cbe25ad8e936f65aa0d6c6dfe583ba
KamilWojas <wojaskamil@yahoo.pl>
2025-02-05 23:40:25 +0100

    Tab1

--- Java od 0/Egzamin/Analiza_Kodu/Tab1.java
+++ Java od 0/Egzamin/Analiza_Kodu/Tab1.java
@@ -1,2 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        int[][] tab = {
+                {2, 4, 6, 8},
+                {1, 3, 5, 7},
+                {10, 12, 14, 16}
+        };
+    }
