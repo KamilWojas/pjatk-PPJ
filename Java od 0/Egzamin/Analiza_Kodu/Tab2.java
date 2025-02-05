052f75592cb29dd4fec657e3187d06946393b513
KamilWojas <wojaskamil@yahoo.pl>
2025-02-05 23:43:45 +0100

    Tab2

--- Java od 0/Egzamin/Analiza_Kodu/Tab2.java
+++ Java od 0/Egzamin/Analiza_Kodu/Tab2.java
@@ -1,4 +1,8 @@
 public class Tab2 {
     public static void main(String[] args) {
-        
-    }
+
+        int[][] numbers = {
+                {5, 10, 15},
+                {20, 25, 30},
+                {35, 40, 45}
+        };
