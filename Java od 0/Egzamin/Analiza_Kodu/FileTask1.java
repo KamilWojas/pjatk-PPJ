3a1aca074400ffd3b0a94b374cb6dba18343b00a
KamilWojas <wojaskamil@yahoo.pl>
2025-02-05 23:35:06 +0100

    FileTask1

--- Java od 0/Egzamin/Analiza_Kodu/FileTask1.java
+++ Java od 0/Egzamin/Analiza_Kodu/FileTask1.java
@@ -2,4 +2,7 @@ class FileTask1 {
     public static void main(String[] args) throws IOException {
         int[] values = { 0b10101010, 0123, 0x4F, 200 };
 
+        FileOutputStream fos = new FileOutputStream("task1.bin");
+
+
     }
