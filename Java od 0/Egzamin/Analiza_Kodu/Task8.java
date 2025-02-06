6dad8cc1153febedc1e5c2433a805add4a985927
KamilWojas <wojaskamil@yahoo.pl>
2025-02-06 23:27:57 +0100

    Task8

--- Java od 0/Egzamin/Analiza_Kodu/Task8.java
+++ Java od 0/Egzamin/Analiza_Kodu/Task8.java
@@ -1,2 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        int[][] matrix = {
+                {11, 22, 33},
+                {44, 55, 66},
+                {77, 88, 99}
+        };
+    }
