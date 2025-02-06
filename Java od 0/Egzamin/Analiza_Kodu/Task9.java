5dbbd67c8ff3f1476c042e14c9d602e221b01599
KamilWojas <wojaskamil@yahoo.pl>
2025-02-06 23:30:48 +0100

    Task9

--- Java od 0/Egzamin/Analiza_Kodu/Task9.java
+++ Java od 0/Egzamin/Analiza_Kodu/Task9.java
@@ -1,2 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        int[][] array = {
+                {9, 8, 7},
+                {6, 5, 4},
+                {3, 2, 1}
+        };
+    }
