98042fe8a3b13b5ff67ffe1879aef8164c8c61ae
KamilWojas <wojaskamil@yahoo.pl>
2025-02-07 22:47:06 +0100

    Task11

--- Java od 0/Egzamin/Analiza_Kodu/Task11.java
+++ Java od 0/Egzamin/Analiza_Kodu/Task11.java
@@ -3,5 +3,7 @@ import java.util.List;
 
 public class Task11 {
     public static void main(String[] args) {
-
-    }
+        List<Zwierze> listaZwierzat = new ArrayList<>();
+        listaZwierzat.add(new Pies());
+        listaZwierzat.add(new Kot());
+        listaZwierzat.add(new Krowa());
