f602257a87accca7f7a0198ae82414f40d06165a
KamilWojas <wojaskamil@yahoo.pl>
2025-02-09 22:44:52 +0100

    Klasa dziedzicząca - płatność kartą

--- Java od 0/Zadania12/Task14.java
+++ Java od 0/Zadania12/Task14.java
@@ -1,3 +1,10 @@
 abstract class Platnosc {
     abstract void wykonajPlatnosc(double kwota);
 }
+
+class PlatnoscKarta extends Platnosc {
+    @Override
+    void wykonajPlatnosc(double kwota) {
+        System.out.println("Płatność kartą na kwotę: " + kwota + " zł.");
+    }
+}
