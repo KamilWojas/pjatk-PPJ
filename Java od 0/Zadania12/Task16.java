d8f8296611d4587bfe39eb1f9fb6f718bdff96b3
KamilWojas <wojaskamil@yahoo.pl>
2025-02-10 23:41:07 +0100

    Zapis do pliku

--- Java od 0/Zadania12/Task16.java
+++ Java od 0/Zadania12/Task16.java
@@ -14,4 +14,6 @@ public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         Osoba osoba = new Osoba("Jan");
 
-    }
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("osoba.dat"));
+        out.writeObject(osoba);
+        out.close();
