150e6b7f27b963dc4f5b2a867796d35ff0e0d38a
KamilWojas <wojaskamil@yahoo.pl>
2025-02-10 23:43:07 +0100

    Task17

--- Java od 0/Zadania12/Task17.java
+++ Java od 0/Zadania12/Task17.java
@@ -4,3 +4,8 @@ import java.util.List;
 class Osoba {
     private String imie;
     private static List<Osoba> ekstensja = new ArrayList<>();
+
+    public Osoba(String imie) {
+        this.imie = imie;
+        ekstensja.add(this);
+    }
