9205e666de8b6456b3ad038fbdbdb636f0e37a58
KamilWojas <wojaskamil@yahoo.pl>
2025-02-10 23:40:01 +0100

    Klasa do serializacji

--- Java od 0/Zadania12/Task16.java
+++ Java od 0/Zadania12/Task16.java
@@ -4,4 +4,8 @@ import java.io.*;
 class Osoba implements Serializable {
     private static final long serialVersionUID = 1L;
     String imie;
+
+    public Osoba(String imie) {
+        this.imie = imie;
+    }
 }
