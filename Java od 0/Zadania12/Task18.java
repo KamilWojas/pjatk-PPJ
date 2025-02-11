447a717332dd823cd4135e4fd363f6f625cc6f89
KamilWojas <wojaskamil@yahoo.pl>
2025-02-11 22:58:55 +0100

    Task18

--- Java od 0/Zadania12/Task18.java
+++ Java od 0/Zadania12/Task18.java
@@ -3,3 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Task18 implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String imie;
+    private static List<Osoba> ekstensja = new ArrayList<>();
+}
