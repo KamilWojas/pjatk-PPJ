126aad4cd255f516a7c767d2d79a54728d573749
KamilWojas <wojaskamil@yahoo.pl>
2025-02-11 23:01:15 +0100

    Task19

--- Java od 0/Zadania12/Task19.java
+++ Java od 0/Zadania12/Task19.java
@@ -2,3 +2,9 @@ class Adres {
     String ulica;
     public Adres(String ulica) { this.ulica = ulica; }
 }
+
+class Task19 {
+    private String imie;
+    private Adres adres;
+    private static int liczbaOsob = 0; 
+    private List<String> telefony = new ArrayList<>();
