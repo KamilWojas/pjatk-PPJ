4ed0d510509a4284a34454e32954bc998fc43a6e
KamilWojas <wojaskamil@yahoo.pl>
2025-02-12 22:41:02 +0100

    Task22

--- Java od 0/Zadania12/Task22.java
+++ Java od 0/Zadania12/Task22.java
@@ -6,5 +6,7 @@ public class Task22 {
         int wynik1 = liczba >> 0; // Przesunięcie o 0 nie zmienia liczby
         System.out.println("1. Przesunięcie o 0 w prawo: " + wynik1 + " (binarnie: " + Integer.toBinaryString(wynik1) + ")");
 
+        // 2. Rezultat poprzedniego punktu przesunięty o 2 pozycje w lewo
+        int wynik2 = wynik1 << 2; // Przesunięcie o 2 w lewo
 
     }
