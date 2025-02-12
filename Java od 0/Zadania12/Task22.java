c4f7db7da92c7fe09f7fee62ad91d989ece72456
KamilWojas <wojaskamil@yahoo.pl>
2025-02-12 22:40:36 +0100

    Task22

--- Java od 0/Zadania12/Task22.java
+++ Java od 0/Zadania12/Task22.java
@@ -2,4 +2,7 @@ public class Task22 {
     public static void main(String[] args) {
         int liczba = 14; // 14 w systemie dziesiętnym (binarnie: 1110)
 
+        // 1. Przesunięcie o zero pozycji w prawo
+        int wynik1 = liczba >> 0; // Przesunięcie o 0 nie zmienia liczby
+
     }
