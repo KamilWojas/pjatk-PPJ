d81b000a922ad5240c4f2767d7afb5ce73ca0442
KamilWojas <wojaskamil@yahoo.pl>
2025-02-12 22:38:35 +0100

    Task21

--- Java od 0/Zadania12/Task21.java
+++ Java od 0/Zadania12/Task21.java
@@ -1,5 +1,6 @@
 public class Task21 {
     public static void main(String[] args) {
         int wPrawo = 0b1110 >> 0;
+        System.out.println("Przesunięcie o 0 w prawo: " + wPrawo); // Wynik: 14
 
     }
