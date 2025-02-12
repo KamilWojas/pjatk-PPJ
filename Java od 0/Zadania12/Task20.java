4fb9a2f6ecad3d108183e690a1c29d2e3384da51
KamilWojas <wojaskamil@yahoo.pl>
2025-02-12 22:36:46 +0100

    Task20

--- Java od 0/Zadania12/Task20.java
+++ Java od 0/Zadania12/Task20.java
@@ -1,8 +1,10 @@
 public class Task20 {
     public static void main(String[] args) {
         int liczba = 250;
-
         int ostatnie5Bitow = liczba & 0b11111;
+
+        System.out.println("Liczba: " + liczba + " (binarnie: " + Integer.toBinaryString(liczba) + ")");
+
 
 
     }
