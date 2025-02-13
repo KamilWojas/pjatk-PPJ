e8bdd8ab86928b7214e8d9d8c8b0daa592fcd68d
KamilWojas <wojaskamil@yahoo.pl>
2025-02-13 22:25:28 +0100

    Task25

--- Java od 0/Zadania12/Task25.java
+++ Java od 0/Zadania12/Task25.java
@@ -7,3 +7,6 @@ public class Task25 {
         // Pobranie liczby od użytkownika
         System.out.print("Podaj liczbę: ");
         int liczba = scanner.nextInt();
+
+        // Operator warunkowy
+        String wynik = (liczba % 2 == 0) ? "Parzysta" : "Nieparzysta";
