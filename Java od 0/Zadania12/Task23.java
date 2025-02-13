7cfd2db8bf4d2c23261252429ed40a05d9d19635
KamilWojas <wojaskamil@yahoo.pl>
2025-02-13 22:21:15 +0100

    Task23

--- Java od 0/Zadania12/Task23.java
+++ Java od 0/Zadania12/Task23.java
@@ -4,4 +4,7 @@ public class Task23 {
 
         // Wyciągamy ostatnie 5 bitów za pomocą maski 0b11111
         int ostatnie5Bitow = wartosc & 0b11111; // AND z maską 0b11111
+
+        // Wyświetlamy wyniki
+        System.out.println("Wartość: " + wartosc + " (binarnie: " + Integer.toBinaryString(wartosc) + ")");
     }
