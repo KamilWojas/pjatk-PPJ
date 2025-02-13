224d98e8e80070006a137776decfc3ea4b97356e
KamilWojas <wojaskamil@yahoo.pl>
2025-02-13 22:25:14 +0100

    Task25

--- Java od 0/Zadania12/Task25.java
+++ Java od 0/Zadania12/Task25.java
@@ -3,3 +3,7 @@ import java.util.Scanner;
 public class Task25 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        // Pobranie liczby od użytkownika
+        System.out.print("Podaj liczbę: ");
+        int liczba = scanner.nextInt();
