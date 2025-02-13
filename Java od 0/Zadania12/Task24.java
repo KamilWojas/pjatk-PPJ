da4933897511b5c3cb73fac415af107fabf779b7
KamilWojas <wojaskamil@yahoo.pl>
2025-02-13 22:23:13 +0100

    Task24

--- Java od 0/Zadania12/Task24.java
+++ Java od 0/Zadania12/Task24.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 
 public class Task24 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+
+        // Pobranie dwóch liczb od użytkownika
+        System.out.print("Podaj pierwszą liczbę: ");
+        int liczba1 = scanner.nextInt();
 
     }
