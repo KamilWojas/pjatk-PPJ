a52c2b23f6978f47ba50d6bbeae033b2f33c774d
KamilWojas <wojaskamil@yahoo.pl>
2025-02-14 23:23:05 +0100

    Project

--- Java od 0/Zadania12/Project.java
+++ Java od 0/Zadania12/Project.java
@@ -2,3 +2,7 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 class Project {
+    private Optional<LocalDate> paymentDate = Optional.empty(); // Opcjonalna data płatności
+
+    public void setPaymentDate(LocalDate date) {
+}
