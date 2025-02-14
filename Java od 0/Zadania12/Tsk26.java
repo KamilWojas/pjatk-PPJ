7103a090b66fccc4d2c699df02cfcf243ae64733
KamilWojas <wojaskamil@yahoo.pl>
2025-02-14 23:24:44 +0100

    Task26

--- Java od 0/Zadania12/Tsk26.java
+++ Java od 0/Zadania12/Tsk26.java
@@ -4,3 +4,8 @@ import java.util.List;
 class Task26 {
     private String name;
     private double bonus;
+
+    public Task26(String name, double bonus) {
+        this.name = name;
+        this.bonus = bonus;
+    }
