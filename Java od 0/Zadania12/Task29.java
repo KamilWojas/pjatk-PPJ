0b88b02d952df5b0b6cb4f9d54ef37aeab00289b
KamilWojas <wojaskamil@yahoo.pl>
2025-02-15 23:05:55 +0100

    Task29

--- Java od 0/Zadania12/Task29.java
+++ Java od 0/Zadania12/Task29.java
@@ -2,3 +2,8 @@ class Task29 {
     private String name;
     private Programmer assignedProgrammer;
     private Designer assignedDesigner;
+
+    public Task29(String name, Programmer programmer, Designer designer) {
+        if (programmer != null && designer != null) {
+            throw new IllegalArgumentException("Task może być przypisany do Programmer LUB Designer, ale nie do obu jednocześnie (XOR constraint).");
+        }
