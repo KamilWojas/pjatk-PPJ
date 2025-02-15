5da5436fbe842bb4766d4c6cc1d94d4cb55bb65d
KamilWojas <wojaskamil@yahoo.pl>
2025-02-15 23:03:52 +0100

    Task28

--- Java od 0/Zadania12/Task28.java
+++ Java od 0/Zadania12/Task28.java
@@ -2,4 +2,8 @@ import java.util.Optional;
 
 class Task28 {
     private Optional<Double> finishedTaskBonus = Optional.empty();
+
+    public Task28(Double finishedTaskBonus) {
+        this.finishedTaskBonus = Optional.ofNullable(finishedTaskBonus);
+    }
 }
