a5121b03d1b913530a3203ea7c3fdba8d893581b
KamilWojas <wojaskamil@yahoo.pl>
2025-02-15 23:07:51 +0100

    Task30

--- Java od 0/Zadania12/Task30.java
+++ Java od 0/Zadania12/Task30.java
@@ -9,3 +9,8 @@ class PrivateCustomer {
 class Task30 {
     private CorporateCustomer corporateCustomer;
     private PrivateCustomer privateCustomer;
+
+    public Task30(CorporateCustomer corporateCustomer, PrivateCustomer privateCustomer) {
+        if (corporateCustomer != null && privateCustomer != null) {
+            throw new IllegalArgumentException("Project can have either CorporateCustomer OR PrivateCustomer, not both (XOR constraint)");
+        }
