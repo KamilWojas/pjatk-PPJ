d13d78197bad5010a909d8ffc5c691ba25816585
KamilWojas <wojaskamil@yahoo.pl>
2025-02-16 23:10:34 +0100

    Task31

--- Java od 0/Zadania12/Task31.java
+++ Java od 0/Zadania12/Task31.java
@@ -11,3 +11,13 @@ class Aktor {
         return imieNazwisko;
     }
 }
+
+// Klasa reprezentująca Film, implementująca asocjację kwalifikowaną
+class Film {
+    // Mapa: kluczem jest tytuł filmu, wartością - lista aktorów grających w tym filmie
+    private static Map<String, List<Aktor>> filmy = new HashMap<>();
+
+    // Dodanie aktora do filmu
+    public static void dodajAktorDoFilmu(String tytul, Aktor aktor) {
+        filmy.computeIfAbsent(tytul, k -> new ArrayList<>()).add(aktor);
+    }
