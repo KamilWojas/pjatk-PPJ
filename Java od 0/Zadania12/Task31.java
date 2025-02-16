fbd3bdc24e2867974013323e2395331e308c2dc4
KamilWojas <wojaskamil@yahoo.pl>
2025-02-16 23:10:16 +0100

    Task31

---
+++ Java od 0/Zadania12/Task31.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Aktor {
+    private String imieNazwisko;
+
+    public Aktor(String imieNazwisko) {
+        this.imieNazwisko = imieNazwisko;
+    }
+
+    public String getImieNazwisko() {
+        return imieNazwisko;
+    }
+}
