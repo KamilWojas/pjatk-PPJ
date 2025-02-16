9a6c7ffba4026958f0165e7b5d2dc30190c4d4ec
KamilWojas <wojaskamil@yahoo.pl>
2025-02-16 23:12:50 +0100

    Task32

---
+++ Java od 0/Zadania12/Task32.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Aktor {
+    private String imieNazwisko;
+
+    public Aktor(String imieNazwisko) {
+        this.imieNazwisko = imieNazwisko;
+    }
+
+    public String getImieNazwisko() {
+        return imieNazwisko;
+    }
+}
