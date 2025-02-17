fbcc860d96a3d6350a2d42b0c550e9e7fbf81e8c
KamilWojas <wojaskamil@yahoo.pl>
2025-02-17 22:37:26 +0100

    Task34

---
+++ Java od 0/Zadania12/Task34.java
@@ -0,0 +1,5 @@
+public class Task34 {
+    public static void main(String[] args) {
+        Pracownik pracownik1 = new Pracownik("Jan Kowalski");
+        Projekt projekt1 = new Projekt("Projekt AI");
+        Dzial dzial1 = new Dzial("IT");
