94f4b9d1aacdcbcb12b3985a677fcf7aa50c3b77
KamilWojas <wojaskamil@yahoo.pl>
2025-02-17 22:41:47 +0100

    Klient

--- Java od 0/Zadania12/Klient.java
+++ Java od 0/Zadania12/Klient.java
@@ -13,3 +13,11 @@ class Klient {
     public void dodajProdukt(Produkt produkt) {
         koszyk.put(produkt, koszyk.getOrDefault(produkt, 0) + 1);
     }
+
+    public void wyswietlKoszyk() {
+        System.out.println("Koszyk klienta: " + imie);
+        for (Map.Entry<Produkt, Integer> entry : koszyk.entrySet()) {
+            System.out.println(entry.getKey() + " x" + entry.getValue());
+        }
+    }
+}
