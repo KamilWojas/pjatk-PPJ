c8742854bfb6e06e1b9304a1ce58f4c78f222585
KamilWojas <wojaskamil@yahoo.pl>
2025-02-17 22:38:12 +0100

    Task34

--- Java od 0/Zadania12/Task34.java
+++ Java od 0/Zadania12/Task34.java
@@ -10,3 +10,8 @@ public class Task34 {
         try {
             pracownik1.przypiszDoDzialu(dzial1);
         } catch (IllegalStateException e) {
+            System.out.println("Błąd: " + e.getMessage());
+        }
+    }
+}
+        }
