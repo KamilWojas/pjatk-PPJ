8cacf18d441b0d341c3e5f991b59f4b3b3c9a285
KamilWojas <wojaskamil@yahoo.pl>
2025-02-18 23:06:06 +0100

    Produkt

---
+++ Java od 0/Zadania12/Produkt.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Produkt {
+    private String nazwa;
+    private double cena;
+
+    public Produkt(String nazwa, double cena) {
+        this.nazwa = nazwa;
+        this.cena = cena;
+    }
