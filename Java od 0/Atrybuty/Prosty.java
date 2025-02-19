7329fdc17907ea4407a07dcac3cbf756fccf8d74
KamilWojas <wojaskamil@yahoo.pl>
2025-02-19 22:39:45 +0100

    Prosty

---
+++ Java od 0/Atrybuty/Prosty.java
@@ -0,0 +1,10 @@
+public class Prosty{
+
+    private String imie; //atrybut prosty
+    private int wiek;
+
+    public Prosty(String imie, int wiek){
+        this.imie = imie;
+        this.wiek = wiek;
+    }
+}
