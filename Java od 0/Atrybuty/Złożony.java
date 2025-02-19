8d283d5828ae95c6265dba50f04637f438873340
KamilWojas <wojaskamil@yahoo.pl>
2025-02-19 22:53:33 +0100

    Złożony

--- Java od 0/Atrybuty/Złożony.java
+++ Java od 0/Atrybuty/Złożony.java
@@ -2,4 +2,12 @@ public class Złożony {
     private String ulica;
     private String miasto;
     private String kodPocztowy;
+
+    public Złożony(String ulica, String miasto, String kodPocztowy){
+        this.kodPocztowy = kodPocztowy;
+        this.miasto = miasto;
+        this.ulica = ulica;
+    }
+
+    //Gettery i Settery
 }
