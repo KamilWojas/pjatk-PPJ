13a9f3abb6f9423c8085e2efbca8961a113b577a
KamilWojas <wojaskamil@yahoo.pl>
2025-02-19 22:41:53 +0100

    Prosty

--- Java od 0/Atrybuty/Prosty.java
+++ Java od 0/Atrybuty/Prosty.java
@@ -7,4 +7,22 @@ public class Prosty{
         this.imie = imie;
         this.wiek = wiek;
     }
+
+    //Getter dla imienia
+    public String getImie{}{
+        return imie;
+    }
+    //Setter dla imienia
+    public void setImie(String imie){
+        this.imie = imie;
+    }
+    //Getter dla wieku
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        this.wiek = wiek;
+    }
 }
