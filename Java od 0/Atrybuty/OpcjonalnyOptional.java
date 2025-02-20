ab7ad867cc2e3c0ef0dadc4b586d453dd30189ac
KamilWojas <wojaskamil@yahoo.pl>
2025-02-20 19:12:47 +0100

    Opcjonalny optional

---
+++ Java od 0/Atrybuty/OpcjonalnyOptional.java
@@ -0,0 +1,7 @@
+//Użycie Optional<T> pozwala uniknąć null i ułatwia obsługę opcjonalnych wartości.
+
+public class OpcjonalnyOptional {
+    public String imie;
+    public int wiek;
+    public Optiona<String> numerTelefon; //Opcjonalny nr telefonu
+}
