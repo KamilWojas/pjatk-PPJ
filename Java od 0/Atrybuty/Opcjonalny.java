cd2b9a8438b815e24cc558c20106b325d54fe5ac
KamilWojas <wojaskamil@yahoo.pl>
2025-02-20 18:58:38 +0100

    Opcjonalny

--- Java od 0/Atrybuty/Opcjonalny.java
+++ Java od 0/Atrybuty/Opcjonalny.java
@@ -2,4 +2,16 @@ public class Opcjonalny {
     public String imie;
     public int wiek;
     public String numerTelefonu; //może być null
+
+    public Opcjonalny(String imie, int wiek, String numerTelefonu){
+        this.imie = imie;
+        this.wiek = wiek;
+        this.numerTelefonu = null;
+    }
+
+    public Opcjonalny(String imie, int wiek, String numerTelefonu){
+        this.imie = imie;
+        this.wiek = wiek;
+        this.numerTelefonu = numerTelefonu;
+    }
 }
