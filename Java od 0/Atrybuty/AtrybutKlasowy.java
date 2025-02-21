31f1024a905950ec9b7d88631fd770acf789918c
KamilWojas <wojaskamil@yahoo.pl>
2025-02-21 20:07:41 +0100

    Atrybut klasowy

--- Java od 0/Atrybuty/AtrybutKlasowy.java
+++ Java od 0/Atrybuty/AtrybutKlasowy.java
@@ -2,4 +2,12 @@ public class AtrybutKlasowy {
     public static int liczbaKol = 4; //Atrybut klasowy
 
     private String marka; //Atrybut instancji
+
+    public AtrybutKlasowy(String marka){
+        this.marka = marka;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
 }
