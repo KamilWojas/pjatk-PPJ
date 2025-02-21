303d71820a52618df6fc1b677fdab4781a727bb0
KamilWojas <wojaskamil@yahoo.pl>
2025-02-21 19:53:54 +0100

    Pochodny

--- Java od 0/Atrybuty/Pochodny.java
+++ Java od 0/Atrybuty/Pochodny.java
@@ -4,4 +4,14 @@ import java.time.Period;
 public class Pochodny {
     private String imie;
     private LocalDate dataUrodzenia; //Atrybut pochodny
+
+    public Pochodny(String imie, LocalDate dataUrodzenia){
+        this.imie = imie;
+        this.dataUrodzenia = dataUrodzenia;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
 }
