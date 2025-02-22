eee158360c0ce3908700d1b10e5f6e465ef39d86
KamilWojas <wojaskamil@yahoo.pl>
2025-02-22 22:59:10 +0100

    Uniklany

--- Java od 0/Atrybuty/Unikalny.java
+++ Java od 0/Atrybuty/Unikalny.java
@@ -3,4 +3,7 @@ import java.util.Set;
 
 public class Unikalny {
     private String Set<String> istniejacePESELE = new HashSet<>();
+    private String PESEL;
+    private String imie;
+    private String nazwisko;
 }
