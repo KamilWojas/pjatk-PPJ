e3a45d2bfb1218bbbeef89ac891e04d5dd50b360
KamilWojas <wojaskamil@yahoo.pl>
2025-02-23 23:46:58 +0100

    Subset

--- Java od 0/Ograniczenia /Subset.java
+++ Java od 0/Ograniczenia /Subset.java
@@ -2,5 +2,9 @@ public class Subset {
     private String imie;
     private String nazwisko;
     private Set<Projekt> projekty = new HashSer<>();
-    
+
+    private void dodajProjekt(Projekt projekt){
+        projekty.add(projekt);
+        projekt.dodajUczestnika(this);
+    }
 }
