0e08af4262c8f95859a256609a35536f9b3f123b
KamilWojas <wojaskamil@yahoo.pl>
2025-02-24 22:50:14 +0100

    Subset

--- Java od 0/Ograniczenia /Pracownik.java
+++ Java od 0/Ograniczenia /Pracownik.java
@@ -4,4 +4,9 @@ import java.util.Set;
 public class Pracownik {
     private String imie;
     private String nazwisko;
+
+    public Pracownik(String imie, String nazwisko){
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
 }
