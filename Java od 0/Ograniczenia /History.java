291cf75679881d852bb73189183e9285e202f09d
KamilWojas <wojaskamil@yahoo.pl>
2025-02-25 23:00:31 +0100

    History

---
+++ Java od 0/Ograniczenia /History.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class History {
+    private String imie;
+    private String nazwisko;
+    private List<Członkowstwo> historiaCzłonkostwa = new ArrayList<>();
+}
