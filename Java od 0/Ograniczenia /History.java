0a35643c3f82731a2ce19737923ac0064a5fb066
KamilWojas <wojaskamil@yahoo.pl>
2025-02-25 23:09:38 +0100

    History

--- Java od 0/Ograniczenia /History.java
+++ Java od 0/Ograniczenia /History.java
@@ -7,21 +7,35 @@ public class History {
     private String nazwisko;
     private List<Czlonkowstwo> historiaCzlonkostwa = new ArrayList<>();
 
-    public History(String imie, String nazwisko){
+    public History(String imie, String nazwisko) {
         this.imie = imie;
         this.nazwisko = nazwisko;
     }
-    public void dodaDoGrupy(Grupa grupa, Date dataRozpoczecia){
+
+    public void dodaDoGrupy(Grupa grupa, Date dataRozpoczecia) {
         Czlonkostwo czlonkostwo = new Czlonkostwo(this, grupa, dataRozpoczecia);
         historiaCzlonkostwa.add(czlonkostwo);
         grupa.dodajCzlonkostwo(czlonkostwo);
     }
 
-    public void opuscGrupe(Grupa grupa, Date dataZakonczenia){
+    public void opuscGrupe(Grupa grupa, Date dataZakonczenia) {
         for (Czlonkostwo czlonkostwo : historiaCzlonkostwa) {
             if (czlonkostwo.getGrupa().equals(grupa) && czlonkostwo.getDataZakonczenia() == null) {
                 czlonkostwo.setDataZakonczenia(dataZakonczenia);
                 break;
             }
         }
+    }
+}
+
+class Grupa {
+    private String nazwa;
+    private List<Czlonkostwo> czlonkowie = new ArrayList<>();
+
+    public Grupa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public void dodajCzlonkostwo(Czlonkostwo czlonkostwo) {
+        czlonkowie.add(czlonkostwo);
     }
