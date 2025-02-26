58bebdb009bd3ed7feb3c78b6e1a6c65cd6cf3b5
KamilWojas <wojaskamil@yahoo.pl>
2025-02-26 22:40:28 +0100

    Disjoint

---
+++ Java od 0/Dziedziczenie/Disjoint.java
@@ -0,0 +1,11 @@
+public class Disjoint {
+    private String marka;
+
+    public Disjoint(String marka){
+        this.marka = marka;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+}
