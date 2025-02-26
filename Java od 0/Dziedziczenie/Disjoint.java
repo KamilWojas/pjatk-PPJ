89ab681994c680df222d1e9876fe997037a70495
KamilWojas <wojaskamil@yahoo.pl>
2025-02-26 22:43:28 +0100

    Disjoint

--- Java od 0/Dziedziczenie/Disjoint.java
+++ Java od 0/Dziedziczenie/Disjoint.java
@@ -8,4 +8,15 @@ public class Disjoint {
     public String getMarka() {
         return marka;
     }
+    public abstract void wyswietlInformacje(){
+    }
+
+class Samochod extends Disjoint{
+    private int liczbaDrzwi;
+
+    public Samochod(String marka,int liczbaDrzwi){
+        super(marka);
+        this.liczbaDrzwi = liczbaDrzwi;
+
+    }
 }
