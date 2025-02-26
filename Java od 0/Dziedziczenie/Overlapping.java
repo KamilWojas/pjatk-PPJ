999ad90d3d1c75b5b40d175a8efa6c2046915ccf
KamilWojas <wojaskamil@yahoo.pl>
2025-02-26 23:07:13 +0100

    Overlapping

--- Java od 0/Dziedziczenie/Overlapping.java
+++ Java od 0/Dziedziczenie/Overlapping.java
@@ -30,4 +30,18 @@ class PracownikIT extends Overlapping implements Programista,Menadzer, Tester{
     public PracownikIT(String imie){
         super(imie);
     }
+    @Override
+    public void piszKod() {
+        System.out.println(getImie() + " Pisz kod");
+    }
+
+    @Override
+    public void zarzadzajZespolem() {
+        System.out.println(getImie() + " zarzadzaj zespołem");
+    }
+
+    @Override
+    public void testujAplikacje() {
+        System.out.println(getImie() + " testuje aplikacje");
+    }
 }
