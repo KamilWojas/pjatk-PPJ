d1094fe7cb3a15f778a5fe9901ad3989b96cd6fd
KamilWojas <wojaskamil@yahoo.pl>
2025-02-27 23:41:25 +0100

    Spłaszczenie hierarchii dziedziczenia

---
+++ Java od 0/Spłaszczenie hierarchii/PoajzdPoSplaszczeniu.java
@@ -0,0 +1,9 @@
+class PojazdPoSplaszczeniu {
+    public enum TypPojazdu { SAMOCHOD, MOTOCYKL, ROWER }
+
+    private String marka;
+    private int predkoscMaksymalna;
+    private TypPojazdu typ;
+    private int liczbaDrzwi;  
+    private boolean maKask;   
+    private boolean elektryczny; 
