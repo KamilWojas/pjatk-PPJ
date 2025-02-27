83443ebd1d891e2b0cb51ec97989509907bb1556
KamilWojas <wojaskamil@yahoo.pl>
2025-02-27 23:37:23 +0100

    Spłaszczenie hierarchii dziedziczenia

---
+++ Java od 0/Spłaszczenie hierarchii/Pojazd.java
@@ -0,0 +1,10 @@
+abstract class Pojazd{
+    private String marka;
+    private int predkoscMaksymalna;
+
+    public Pojazd(String marka, int predkoscMaksymalna) {
+        this.marka = marka;
+        this.predkoscMaksymalna = predkoscMaksymalna;
+    }
+
+}
