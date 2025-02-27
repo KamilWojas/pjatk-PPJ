f7cd5f6483cea684459997b96e617f6076f5f635
KamilWojas <wojaskamil@yahoo.pl>
2025-02-27 23:37:53 +0100

    Spłaszczenie hierarchii dziedziczenia

--- Java od 0/Spłaszczenie hierarchii/Pojazd.java
+++ Java od 0/Spłaszczenie hierarchii/Pojazd.java
@@ -18,4 +18,16 @@ abstract class Pojazd{
     public abstract void wyswietlInformacje();
 }
 
+class Samochod extends Pojazd {
+    private int liczbaDrzwi;
+
+    public Samochod(String marka, int predkoscMaksymalna, int liczbaDrzwi) {
+        super(marka, predkoscMaksymalna);
+        this.liczbaDrzwi = liczbaDrzwi;
+    }
+
+    @Override
+    public void wyswietlInformacje() {
+        System.out.println("Samochód marki " + getMarka() + " z " + liczbaDrzwi + " drzwiami, prędkość maksymalna: " + getPredkoscMaksymalna() + " km/h.");
+    }
 }
