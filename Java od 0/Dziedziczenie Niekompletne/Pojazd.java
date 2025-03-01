66a1ea089937f7923fcba22e3c421b2a7715fc6e
KamilWojas <wojaskamil@yahoo.pl>
2025-03-01 23:18:45 +0100

    Dziedziczenie niekompletne

--- Java od 0/Dziedziczenie Niekompletne/Pojazd.java
+++ Java od 0/Dziedziczenie Niekompletne/Pojazd.java
@@ -12,3 +12,16 @@ abstract class Pojazd {
 
     public abstract void wyswietlInformacje();
 }
+class Samochod extends Pojazd {
+    private int liczbaDrzwi;
+
+    public Samochod(String marka, int liczbaDrzwi) {
+        super(marka);
+        this.liczbaDrzwi = liczbaDrzwi;
+    }
+
+    @Override
+    public void wyswietlInformacje() {
+        System.out.println("Samochód marki " + getMarka() + " z " + liczbaDrzwi + " drzwiami.");
+    }
+}
