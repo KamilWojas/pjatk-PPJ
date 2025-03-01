20f4551a2eec1dfbf051262b027d682934298d43
KamilWojas <wojaskamil@yahoo.pl>
2025-03-01 16:22:15 +0100

    Wielodziedziczenie - spłaszczenie

--- Java od 0/Wielodziedziczenie/PracownikIT.java
+++ Java od 0/Wielodziedziczenie/PracownikIT.java
@@ -2,3 +2,9 @@ class PracownikIT {
     private String imie;
     private boolean jestProgramista;
     private boolean jestMenedzerem;
+
+    public PracownikIT(String imie, boolean programista, boolean menedzer) {
+        this.imie = imie;
+        this.jestProgramista = programista;
+        this.jestMenedzerem = menedzer;
+    }
