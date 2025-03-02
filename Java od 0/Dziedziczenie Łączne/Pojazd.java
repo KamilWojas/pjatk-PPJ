264465a02ce8ec566ccb03343e45fe16d0516955
KamilWojas <wojaskamil@yahoo.pl>
2025-03-02 21:48:02 +0100

    dziedziczenie lączne kompozycji?

--- Java od 0/Dziedziczenie Łączne/Pojazd.java
+++ Java od 0/Dziedziczenie Łączne/Pojazd.java
@@ -17,3 +17,17 @@ interface Spalinowy{
 interface Elektryczny{
     void naladuj();
 }
+
+abstract class Pojazd{
+    private String marka;
+
+    public Pojazd(String marka){
+        this.marka = marka;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public abstract void wyswietlInformacje();
+}
