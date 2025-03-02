338d001c18fbaf36cec7cbc475e75845a50081f8
KamilWojas <wojaskamil@yahoo.pl>
2025-03-02 21:45:42 +0100

    dziedziczenie lączne kompozycji?

---
+++ Java od 0/Dziedziczenie Łączne/Pojazd.java
@@ -0,0 +1,11 @@
+class Silnik{
+    private String typ'
+
+        public Silnik(String typ){
+        this.typ = typ;
+        }
+
+        public void uruchom(){
+            System.out.println("Silnik " + typ + " został uruchomiony");
+        }
+}
