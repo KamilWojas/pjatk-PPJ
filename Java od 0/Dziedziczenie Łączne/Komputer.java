3973beee64f79b2d2d3abd24837955bdbd4e16dd
KamilWojas <wojaskamil@yahoo.pl>
2025-03-03 22:40:24 +0100

    dziedziczenie lączne kompozycji?

---
+++ Java od 0/Dziedziczenie Łączne/Komputer.java
@@ -0,0 +1,11 @@
+class Komputer{
+    private String model;
+
+    public Komputer(String model){
+        this.model = model;
+    }
+
+    public void uruchom(){
+        System.out.println("Komputer " + model + " Został uruchomiony ");
+    }
+}
