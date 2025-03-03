879c7ffd139934efc621b6e080bb48e0ec560639
KamilWojas <wojaskamil@yahoo.pl>
2025-03-03 22:54:37 +0100

    dziedziczenie lączne kompozycji?

---
+++ Java od 0/Dziedziczenie Łączne/Klatka.java
@@ -0,0 +1,10 @@
+class Klatka{
+    private int numer;
+
+    public Klatka(int numer){
+        this.numer = numer;
+    }
+    public void otworz(){
+        System.out.println("Klatka nr " + numer + " została otwarta");
+    }
+}
