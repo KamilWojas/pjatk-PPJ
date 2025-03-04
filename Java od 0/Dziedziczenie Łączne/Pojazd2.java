a95bfe595aba60942bc5639afb66ba90bb29b870
KamilWojas <wojaskamil@yahoo.pl>
2025-03-04 23:31:07 +0100

    Dziedziczenie łączne + Kompozycja

--- Java od 0/Dziedziczenie Łączne/Pojazd2.java
+++ Java od 0/Dziedziczenie Łączne/Pojazd2.java
@@ -9,3 +9,16 @@ class Silnik{
         System.out.println("Silnik " + typ + " został uruchomiony");
     }
 }
+
+//overlapping
+interface Ladowe{
+    void jedz();
+}
+
+interface Wodne{
+    void plyn();
+}
+
+interface Latajace{
+    void lec();
+}
