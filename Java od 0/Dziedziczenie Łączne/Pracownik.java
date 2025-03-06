045e4f074f7f5cf08b6f8ca961cc8badb9550369
KamilWojas <wojaskamil@yahoo.pl>
2025-03-06 22:08:41 +0100

    Dziedziczenie łączne + Atrybuty

--- Java od 0/Dziedziczenie Łączne/Pracownik.java
+++ Java od 0/Dziedziczenie Łączne/Pracownik.java
@@ -1,1 +1,12 @@
 import java.util.UUID;
+
+abstract class Pracownik{
+    private final String id;
+    private String imie;
+    private int wiek;
+    private double pensja;
+
+    public Pracownik(String imie, int wiek, double pensja){
+        
+    }
+}
