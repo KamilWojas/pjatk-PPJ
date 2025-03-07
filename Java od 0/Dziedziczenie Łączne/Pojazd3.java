65f17d9d73cdb7d306e31a24b7654dc5bd4cd29b
KamilWojas <wojaskamil@yahoo.pl>
2025-03-07 22:32:21 +0100

    Klasa bazowa z atrybutem pochodnym

--- Java od 0/Dziedziczenie Łączne/Pojazd3.java
+++ Java od 0/Dziedziczenie Łączne/Pojazd3.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.UUID;
 
 abstract class Pojazd3{
-    
+    private final String id;
     private String nazwa;
     private double cenaNetto;
     private double cenaBruttto;
