272bc8acc418d2bfc09248667b902e2f4b9a3e8b
KamilWojas <wojaskamil@yahoo.pl>
2025-03-07 22:37:49 +0100

    Klasa bazowa z atrybutem klasowym

--- Java od 0/Dziedziczenie Łączne/Pojazd3.java
+++ Java od 0/Dziedziczenie Łączne/Pojazd3.java
@@ -3,10 +3,18 @@ import java.util.List;
 import java.util.UUID;
 
 abstract class Pojazd3{
-    private final String id;
-    private String nazwa;
-    private double cenaNetto;
-    private double cenaBruttto;
-    private static int liczbaProduktow = 3; 
-    
+    private double cenaZaDzien;
+    private int liczbaDni;
+
+    // Lista przechowująca wszystkie rezerwacje (dla metody klasowej)
+    private static List<Pojazd> rezerwacje = new ArrayList<>();
+
+    public Pojazd(String marka, double cenaZaDzien, int liczbaDni) {
+        this.marka = marka;
+        this.cenaZaDzien = cenaZaDzien;
+        this.liczbaDni = liczbaDni;
+        rezerwacje.add(this);  // Automatycznie dodaje pojazd do listy rezerwacji
+    }
+
+
 }
