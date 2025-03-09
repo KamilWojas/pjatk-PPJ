9a261bcc0a8c4c20e23f2a06f331a209dcff8eb0
KamilWojas <wojaskamil@yahoo.pl>
2025-03-09 22:42:09 +0100

    Deklaracja konstruktora

--- Java od 0/Zadnia/KontoBankowe.java
+++ Java od 0/Zadnia/KontoBankowe.java
@@ -3,4 +3,9 @@ class KontoBankowe{
     private String nazwiskoWlasciciela;
     private double saldoPoczatkowe;
     
+    public KontoBankowe(String numerKonta, String nazwiskoWlasciciela, double saldoPoczatkowe){
+     this.numerKonta = numerKonta;
+     this.nazwiskoWlasciciela = nazwiskoWlasciciela;
+     this.saldoPoczatkowe = saldoPoczatkowe;
+    }
 }
