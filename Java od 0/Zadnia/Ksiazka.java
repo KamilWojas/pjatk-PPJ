f68f9fb96c583cae7ef84b107a46065dc0807f78
KamilWojas <wojaskamil@yahoo.pl>
2025-03-10 22:39:14 +0100

    Deklaracja konstruktora

--- Java od 0/Zadnia/Ksiazka.java
+++ Java od 0/Zadnia/Ksiazka.java
@@ -2,4 +2,10 @@ class Ksiazka{
     private String tytul;
     private String autor;
     private int rokWydania;
+
+    public Ksiazka(String tytul, String autor, int rokWydania){
+        this.tytul = tytul;
+        this.autor = autor;
+        this.rokWydania = rokWydania;
+    }
 }
