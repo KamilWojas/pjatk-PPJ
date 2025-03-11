a1a51f538fd50693c196c3d4e0546989f5b3c914
KamilWojas <wojaskamil@yahoo.pl>
2025-03-11 23:19:25 +0100

    Scanner + lista

--- Java od 0/Zadnia/Zadanie.java
+++ Java od 0/Zadnia/Zadanie.java
@@ -24,3 +24,6 @@ class Zadanie{
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Zadanie> listaZadan = new ArrayList<>();
+    }
