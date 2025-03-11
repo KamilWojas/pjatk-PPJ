929a0e8d713d2ff193499ecc3b0baa6e7ce59bfd
KamilWojas <wojaskamil@yahoo.pl>
2025-03-11 23:31:02 +0100

    Wyświetlanie menu

--- Java od 0/Zadnia/Kalkulator.java
+++ Java od 0/Zadnia/Kalkulator.java
@@ -4,3 +4,16 @@ import java.util.Scanner;
 public class Kalkulator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            
+            System.out.println("\nWybierz operację:");
+            System.out.println("1. Dodawanie (+)");
+            System.out.println("2. Odejmowanie (-)");
+            System.out.println("3. Mnożenie (*)");
+            System.out.println("4. Dzielenie (/)");
+            System.out.println("5. Potęgowanie (^)");
+            System.out.println("6. Pierwiastek kwadratowy (√)");
+            System.out.println("7. Zakończ program");
+
+            int opcja = scanner.nextInt();
