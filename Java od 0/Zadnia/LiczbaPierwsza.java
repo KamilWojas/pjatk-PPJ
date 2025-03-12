502205ca4e1a60194c2f0e98e3ebd755ac18af8a
KamilWojas <wojaskamil@yahoo.pl>
2025-03-12 23:00:13 +0100

    Klasa bazowa

--- Java od 0/Zadnia/LiczbaPierwsza.java
+++ Java od 0/Zadnia/LiczbaPierwsza.java
@@ -3,3 +3,6 @@ import java.util.Scanner;
 public class LiczbaPierwsza {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj liczbę: ");
+        int liczba = scanner.nextInt();
