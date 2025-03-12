122e996fd17ba1f48a0a7cb982f8b889c55515af
KamilWojas <wojaskamil@yahoo.pl>
2025-03-12 23:02:53 +0100

    Jeśli liczba dzieli się przez inne liczby, nie jest pierwsza

--- Java od 0/Zadnia/LiczbaPierwsza.java
+++ Java od 0/Zadnia/LiczbaPierwsza.java
@@ -21,4 +21,7 @@ public class LiczbaPierwsza {
     public static boolean czyLiczbaPierwsza(int n) {
         if (n < 2) return false;
         for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
         }
