3d65a76e7a9c9ec3279ab903e899bf4e19ed2086
KamilWojas <wojaskamil@yahoo.pl>
2025-03-12 23:05:46 +0100

    Pobranie tekstu od użytkownika

--- Java od 0/Zadnia/OdwroconyTekst.java
+++ Java od 0/Zadnia/OdwroconyTekst.java
@@ -3,3 +3,6 @@ import java.util.Scanner;
 public class OdwroconyTekst {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj tekst: ");
+        String tekst = scanner.nextLine();
