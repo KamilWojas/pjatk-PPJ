2038ba9545b1fd04c57a77349a5fb64d6cb45f16
KamilWojas <wojaskamil@yahoo.pl>
2025-03-13 17:28:47 +0100

    Pobranie tekstu od użytkownika

--- Java od 0/Zadnia/Palindrom.java
+++ Java od 0/Zadnia/Palindrom.java
@@ -3,3 +3,7 @@ import java.util.Scanner;
 public class Palindrom {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Podaj tekst: ");
+        String tekst = scanner.nextLine();
