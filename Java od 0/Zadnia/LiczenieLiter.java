bfe46042d1b76b00cb12e139f46b2d84e80a514c
KamilWojas <wojaskamil@yahoo.pl>
2025-03-13 17:24:16 +0100

    Pobranie tekstu od użytkownika

--- Java od 0/Zadnia/LiczenieLiter.java
+++ Java od 0/Zadnia/LiczenieLiter.java
@@ -3,3 +3,5 @@ import java.util.Scanner;
 public class LiczenieLiter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj tekst: ");
