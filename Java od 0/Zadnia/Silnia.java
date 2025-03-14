49ff336dc960ed1743c02849706caaa80a53d166
KamilWojas <wojaskamil@yahoo.pl>
2025-03-14 22:33:27 +0100

    Pobranie liczby od użytkownika

--- Java od 0/Zadnia/Silnia.java
+++ Java od 0/Zadnia/Silnia.java
@@ -3,3 +3,15 @@ import java.util.Scanner;
 public class Silnia {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj liczbę: ");
+        int n = scanner.nextInt();
+
+        if (n < 0) {
+            System.out.println("Nie można obliczyć silni z liczby ujemnej!");
+        } else {
+            System.out.println("Silnia " + n + "! = " + obliczSilnie(n));
+        }
+
+        scanner.close();
+    }
