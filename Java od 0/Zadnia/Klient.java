9192c83281ae1142b50bdc6a6b9f86b7b580595c
KamilWojas <wojaskamil@yahoo.pl>
2025-03-15 23:19:02 +0100

    Klient

--- Java od 0/Zadnia/Klient.java
+++ Java od 0/Zadnia/Klient.java
@@ -4,3 +4,8 @@ import java.util.Map;
 class Klient {
     private String imie;
     private Map<Produkt, Integer> koszyk;
+
+    public Klient(String imie) {
+        this.imie = imie;
+        this.koszyk = new HashMap<>();
+    }
