85d63bccf97351d099cda60b6a05616146d7f739
KamilWojas <wojaskamil@yahoo.pl>
2025-03-15 23:23:38 +0100

    Klient

--- Java od 0/Zadnia/AtrybutKlasowy.java
+++ Java od 0/Zadnia/AtrybutKlasowy.java
@@ -10,3 +10,15 @@ public class AtrybutKlasowy {
         return marka;
     }
 }
+public class Main{
+    public static void main(String[] args) {
+        AtrybutKlasowy auto1 = new AtrybutKlasowy("Toyota");
+        AtrybutKlasowy auto2 = new AtrybutKlasowy("Honda");
+
+        System.out.println(AtrybutKlasowy.liczbaKol);
+
+        AtrybutKlasowy.liczbaKol = 3;
+
+        System.out.println(AtrybutKlasowy.liczbaKol);
+    }
+}
