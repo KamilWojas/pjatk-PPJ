e84ee84241e300f9b3bc135abb9e90e706d103f1
KamilWojas <wojaskamil@yahoo.pl>
2025-03-15 23:27:00 +0100

    ZliczanieZnakow

--- Java od 0/Zadnia/ZliczanieZnakow.java
+++ Java od 0/Zadnia/ZliczanieZnakow.java
@@ -5,3 +5,14 @@ import java.util.Map;
 
 public class ZliczanieZnakow {
     public static void main(String[] args) {
+        String sciezkaDoPliku = "sciezka_do_twojego_pliku.txt";
+
+        Map<Character, Integer> mapaZnakow = new HashMap<>();
+
+        try (FileInputStream fileInputStream = new FileInputStream(sciezkaDoPliku)) {
+            int odczytanyBajt;
+            while ((odczytanyBajt = fileInputStream.read()) != -1) {
+                char znak = (char) odczytanyBajt;
+                mapaZnakow.put(znak, mapaZnakow.getOrDefault(znak, 0) + 1);
+            }
+    }
