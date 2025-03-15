14f528ef7a7314795657a8ba91c6049d486f6ecd
KamilWojas <wojaskamil@yahoo.pl>
2025-03-15 23:26:43 +0100

    ZliczanieZnakow

---
+++ Java od 0/Zadnia/ZliczanieZnakow.java
@@ -0,0 +1,7 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ZliczanieZnakow {
+    public static void main(String[] args) {
