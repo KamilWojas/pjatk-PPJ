faf52ae8860040ec9692eea970f0700bed980ba3
KamilWojas <wojaskamil@yahoo.pl>
2025-03-16 23:48:38 +0100

    Pobranie liczby elementów

--- Java od 0/Zadnia/MinMaxTablica.java
+++ Java od 0/Zadnia/MinMaxTablica.java
@@ -3,3 +3,13 @@ import java.util.Scanner;
 public class MinMaxTablica {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj ilość liczb: ");
+        int n = scanner.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Ilość liczb musi być większa niż 0!");
+            return;
+        }
+
+        int[] liczby = new int[n];
