78d4296dbe1d816063111646a8722ddbbd5c0a46
KamilWojas <wojaskamil@yahoo.pl>
2025-03-16 23:41:59 +0100

    Pobranie liczby od użytkownika

--- Java od 0/Zadnia/SumaCyfr.java
+++ Java od 0/Zadnia/SumaCyfr.java
@@ -3,3 +3,6 @@ import java.util.Scanner;
 public class SumaCyfr {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj liczbę: ");
+        int n = scanner.nextInt();
