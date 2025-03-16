7a28ba2a2e50ea2089cceb282f9701aa297b6050
KamilWojas <wojaskamil@yahoo.pl>
2025-03-16 23:45:52 +0100

    Pobranie liczby od użytkownika

--- Java od 0/Zadnia/LiczbaDoskonala.java
+++ Java od 0/Zadnia/LiczbaDoskonala.java
@@ -3,3 +3,17 @@ import java.util.Scanner;
 public class LiczbaDoskonala {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj liczbę: ");
+        int n = scanner.nextInt();
+
+        if (n < 1) {
+            System.out.println("Liczba musi być większa od 0!");
+        } else if (czyLiczbaDoskonala(n)) {
+            System.out.println("Liczba " + n + " jest liczbą doskonałą.");
+        } else {
+            System.out.println("Liczba " + n + " nie jest liczbą doskonałą.");
+        }
+
+        scanner.close();
+    }
