1b8ecbe2f5b1618f47615f49eded38644ec49b57
KamilWojas <wojaskamil@yahoo.pl>
2025-03-17 19:00:59 +0100

    Sprawdzenie, czy są anagramami

--- Java od 0/Zadnia/Anagramy.java
+++ Java od 0/Zadnia/Anagramy.java
@@ -5,9 +5,18 @@ import java.util.Scanner;
 public class Anagramy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.print("Podaj pierwsze słowo: ");
         String slowo1 = scanner.nextLine().toLowerCase();
 
         System.out.print("Podaj drugie słowo: ");
         String slowo2 = scanner.nextLine().toLowerCase();
+
+        if (czyAnagram(slowo1, slowo2)) {
+            System.out.println("Słowa \"" + slowo1 + "\" i \"" + slowo2 + "\" są anagramami.");
+        } else {
+            System.out.println("Słowa \"" + slowo1 + "\" i \"" + slowo2 + "\" nie są anagramami.");
+        }
+
+        scanner.close();
+    }
