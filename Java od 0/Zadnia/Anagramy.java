6581672cb3e224f40f7e774bb2c577c440ee5e89
KamilWojas <wojaskamil@yahoo.pl>
2025-03-17 19:00:07 +0100

    Pobranie dwóch słów od użytkownika

--- Java od 0/Zadnia/Anagramy.java
+++ Java od 0/Zadnia/Anagramy.java
@@ -5,5 +5,9 @@ import java.util.Scanner;
 public class Anagramy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Podaj pierwsze słowo: ");
+        String slowo1 = scanner.nextLine().toLowerCase();
 
-    }
+        System.out.print("Podaj drugie słowo: ");
+        String slowo2 = scanner.nextLine().toLowerCase();
