048561a594baf85e4619016adca60c997b20fd1a
KamilWojas <wojaskamil@yahoo.pl>
2025-03-17 23:48:47 +0100

    Pobranie tekstu od użytkownika

--- Java od 0/Zadnia/SzyfrCezara.java
+++ Java od 0/Zadnia/SzyfrCezara.java
@@ -3,3 +3,6 @@ import java.util.Scanner;
 public class SzyfrCezara {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj tekst: ");
+        String tekst = scanner.nextLine();
