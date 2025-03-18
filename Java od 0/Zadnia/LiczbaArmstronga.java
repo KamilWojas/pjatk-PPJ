1389aa73e2a3dc74beafde3ac93d0a9aec3979ea
KamilWojas <wojaskamil@yahoo.pl>
2025-03-18 21:56:59 +0100

    Sprawdzenie, czy liczba jest Armstronga

--- Java od 0/Zadnia/LiczbaArmstronga.java
+++ Java od 0/Zadnia/LiczbaArmstronga.java
@@ -6,3 +6,12 @@ public class LiczbaArmstronga {
 
         System.out.print("Podaj liczbę: ");
         int n = scanner.nextInt();
+
+        if (czyLiczbaArmstronga(n)) {
+            System.out.println("Liczba " + n + " jest liczbą Armstronga.");
+        } else {
+            System.out.println("Liczba " + n + " nie jest liczbą Armstronga.");
+        }
+
+        scanner.close();
+    }
