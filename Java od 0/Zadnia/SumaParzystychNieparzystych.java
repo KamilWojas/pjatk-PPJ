970893424a55bd2bd317f9593a8da57f178c3c23
KamilWojas <wojaskamil@yahoo.pl>
2025-03-19 22:38:12 +0100

    Pobranie liczby elementów

--- Java od 0/Zadnia/SumaParzystychNieparzystych.java
+++ Java od 0/Zadnia/SumaParzystychNieparzystych.java
@@ -4,3 +4,14 @@ import java.util.Scanner;
 public class SumaParzystychNieparzystych {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj ilość liczb: ");
+        int n = scanner.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Ilość liczb musi być większa niż 0!");
+            return;
+        }
+
+        int sumaParzystych = 0;
+        int sumaNieparzystych = 0;
