47ce3db52ec5cee359dd249fda670b26ea1649d9
KamilWojas <wojaskamil@yahoo.pl>
2025-03-19 22:29:17 +0100

    Pobranie liczby od użytkownika

--- Java od 0/Zadnia/OdwroconaLiczba.java
+++ Java od 0/Zadnia/OdwroconaLiczba.java
@@ -3,3 +3,6 @@ import java.util.Scanner;
 public class OdwroconaLiczba {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj liczbę: ");
+        int n = scanner.nextInt();
