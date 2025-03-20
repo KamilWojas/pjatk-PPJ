1bc44b53fb98b1559df3109983f202bc9843fc39
KamilWojas <wojaskamil@yahoo.pl>
2025-03-20 21:54:02 +0100

    Pobranie dwóch liczb od użytkownika

--- Java od 0/Zadnia/SumaPrzedzial.java
+++ Java od 0/Zadnia/SumaPrzedzial.java
@@ -3,3 +3,9 @@ import java.util.Scanner;
 public class SumaPrzedzial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj pierwszą liczbę: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Podaj drugą liczbę: ");
+        int b = scanner.nextInt();
