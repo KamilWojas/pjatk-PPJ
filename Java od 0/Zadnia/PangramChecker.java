2b4cdbcc0d9eaf9e79d2c216cb0e130a561ecc2b
KamilWojas <wojaskamil@yahoo.pl>
2025-03-20 21:56:47 +0100

    Pobranie tekstu od użytkownika

--- Java od 0/Zadnia/PangramChecker.java
+++ Java od 0/Zadnia/PangramChecker.java
@@ -4,3 +4,6 @@ import java.util.HashSet;
 public class PangramChecker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj tekst: ");
+        String tekst = scanner.nextLine();
