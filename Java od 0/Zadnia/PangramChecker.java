2b70d7fad28fed639128a04f4181de363515198d
KamilWojas <wojaskamil@yahoo.pl>
2025-03-20 21:56:30 +0100

    Napisz program w języku Java, który:
    Pobiera tekst od użytkownika.
    Sprawdza, czy tekst jest pangramem – czyli czy zawiera wszystkie litery alfabetu przynajmniej raz.
    Wyświetla odpowiedni komunikat.

---
+++ Java od 0/Zadnia/PangramChecker.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+import java.util.HashSet;
+
+public class PangramChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
