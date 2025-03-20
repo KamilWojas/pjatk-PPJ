709b07dab91e66a3cdaa04abf7b276c3df472251
KamilWojas <wojaskamil@yahoo.pl>
2025-03-20 21:49:11 +0100

    Pobranie liczby od użytkownika

--- Java od 0/Zadnia/LiczbyPierwsze.java
+++ Java od 0/Zadnia/LiczbyPierwsze.java
@@ -3,3 +3,16 @@ import java.util.Scanner;
 public class LiczbyPierwsze {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj liczbę: ");
+        int n = scanner.nextInt();
+
+        if (n < 2) {
+            System.out.println("Brak liczb pierwszych w tym zakresie.");
+        } else {
+            System.out.print("Liczby pierwsze od 1 do " + n + ": ");
+            wypiszLiczbyPierwsze(n);
+        }
+
+        scanner.close();
+    }
