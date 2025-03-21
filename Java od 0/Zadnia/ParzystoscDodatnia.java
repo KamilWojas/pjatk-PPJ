b378b1b649940b0cd71ddf51841e45c82084c08c
KamilWojas <wojaskamil@yahoo.pl>
2025-03-21 23:21:21 +0100

    Odczytujemy liczbę typu int

--- Java od 0/Zadnia/ParzystoscDodatnia.java
+++ Java od 0/Zadnia/ParzystoscDodatnia.java
@@ -2,4 +2,7 @@ import java.util.Scanner;
 
 public class ParzystoscDodatnia {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj liczbę całkowitą: ");
+        int liczba = scanner.nextInt();
