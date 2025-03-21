6cf2ad0b7256fe66c45f6adf82aa73b36bd63a23
KamilWojas <wojaskamil@yahoo.pl>
2025-03-21 23:21:44 +0100

    Sprawdzenie parzystości

--- Java od 0/Zadnia/ParzystoscDodatnia.java
+++ Java od 0/Zadnia/ParzystoscDodatnia.java
@@ -6,3 +6,9 @@ public class ParzystoscDodatnia {
 
         System.out.print("Podaj liczbę całkowitą: ");
         int liczba = scanner.nextInt();
+
+        if (liczba % 2 == 0) {
+            System.out.println("Liczba jest parzysta.");
+        } else {
+            System.out.println("Liczba jest nieparzysta.");
+        }
