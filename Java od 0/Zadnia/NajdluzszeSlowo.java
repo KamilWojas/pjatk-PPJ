dc53578ab6ebbaeaaf2a920188aba939584e13b0
KamilWojas <wojaskamil@yahoo.pl>
2025-03-21 23:26:08 +0100

    Odczytujemy całe zdanie

--- Java od 0/Zadnia/NajdluzszeSlowo.java
+++ Java od 0/Zadnia/NajdluzszeSlowo.java
@@ -3,3 +3,6 @@ import java.util.Scanner;
 public class NajdluzszeSlowo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj zdanie: ");
+        String zdanie = scanner.nextLine(); 
