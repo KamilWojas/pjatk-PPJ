1586a28ed1355523c155a9813bceadab28e61bbf
KamilWojas <wojaskamil@yahoo.pl>
2025-03-21 23:23:35 +0100

    Liczba elementów

--- Java od 0/Zadnia/SredniaLiczb.java
+++ Java od 0/Zadnia/SredniaLiczb.java
@@ -3,3 +3,6 @@ import java.util.Scanner;
 public class SredniaLiczb {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ile liczb chcesz podać? ");
+        int n = scanner.nextInt(); 
