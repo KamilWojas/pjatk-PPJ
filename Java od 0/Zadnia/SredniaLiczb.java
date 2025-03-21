5b35922ca3bb0af4700c299ad300d688121d30d6
KamilWojas <wojaskamil@yahoo.pl>
2025-03-21 23:23:51 +0100

    Przechowuje sumę wszystkich liczb

--- Java od 0/Zadnia/SredniaLiczb.java
+++ Java od 0/Zadnia/SredniaLiczb.java
@@ -5,4 +5,6 @@ public class SredniaLiczb {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Ile liczb chcesz podać? ");
-        int n = scanner.nextInt(); 
+        int n = scanner.nextInt();
+
+        double suma = 0; 
