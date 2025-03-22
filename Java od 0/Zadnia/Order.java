fe0b5b89160ab7ca0db8a808aa3068ecd33f3679
KamilWojas <wojaskamil@yahoo.pl>
2025-03-22 14:46:46 +0100

    Tworzymy listę przykładowych zamówień

--- Java od 0/Zadnia/Order.java
+++ Java od 0/Zadnia/Order.java
@@ -20,3 +20,18 @@ class Order {
 
 public class SystemZamowien {
     public static void main(String[] args) {
+
+        List<Order> zamowienia = new ArrayList<>();
+        zamowienia.add(new Order(1, "Jan Kowalski", 100.0));
+        zamowienia.add(new Order(2, "Anna Nowak", 250.0));
+        zamowienia.add(new Order(3, "Jan Kowalski", 200.0));
+        zamowienia.add(new Order(4, "Maria Zielińska", 300.0));
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj nazwę klienta: ");
+        String klient = scanner.nextLine();
+
+        double suma = 0;
+        boolean znaleziono = false;
+    }
