d2c11803755844ff78f41c9ac8dbb9319a49bfae
KamilWojas <wojaskamil@yahoo.pl>
2025-03-24 23:24:42 +0100

    Lista przykładowych pracowników

--- Java od 0/Zadnia/Employee.java
+++ Java od 0/Zadnia/Employee.java
@@ -20,3 +20,14 @@ class Employee {
         return name + " – " + String.format("%.2f", salary);
     }
 }
+public class SystemPracownikow {
+    public static void main(String[] args) {
+       
+        List<Employee> pracownicy = new ArrayList<>();
+        pracownicy.add(new Employee(1, "Anna Nowak", "HR", 5000));
+        pracownicy.add(new Employee(2, "Jan Kowalski", "IT", 6500));
+        pracownicy.add(new Employee(3, "Kamil Zieliński", "HR", 4700));
+        pracownicy.add(new Employee(4, "Marta Wiśniewska", "IT", 7100));
+        pracownicy.add(new Employee(5, "Piotr Maj", "Marketing", 4300));
+
+        Scanner scanner = new Scanner(System.in);
