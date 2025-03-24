f6c97a2457859a166e8e78cb17eeff98f0e7cc9d
KamilWojas <wojaskamil@yahoo.pl>
2025-03-24 23:23:34 +0100

    Tworzenie klasy Employee z polami: id, name, department, salary.

---
+++ Java od 0/Zadnia/Employee.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Employee {
+    int id;
+    String name;
+    String department;
+    double salary;
+
+    public Employee(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
