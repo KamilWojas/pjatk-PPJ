fa8a34ad7dda6e27bdcfd2eddaba59063d7efe3d
KamilWojas <wojaskamil@yahoo.pl>
2025-03-25 23:17:30 +0100

    dzielenie po spacjach

--- Java od 0/Zadnia/LiczenieSlow.java
+++ Java od 0/Zadnia/LiczenieSlow.java
@@ -6,3 +6,6 @@ public class LiczenieSlow {
 
         System.out.print("Podaj zdanie: ");
         String zdanie = scanner.nextLine().toLowerCase();
+
+        String[] slowa = zdanie.split("\\s+"); 
+        Map<String, Integer> licznik = new HashMap<>();
