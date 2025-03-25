284a9cbb62c3355df9995e560909feae04ae3ccc
KamilWojas <wojaskamil@yahoo.pl>
2025-03-25 23:24:35 +0100

    Inicjacja tablicy

--- Java od 0/Zadnia/ZnajdzWTablicy.java
+++ Java od 0/Zadnia/ZnajdzWTablicy.java
@@ -3,3 +3,8 @@ import java.util.Scanner;
 public class ZnajdzWTablicy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int[] liczby = {3, 7, 12, 25, 33, 42};
+
+        System.out.print("Podaj liczbę do wyszukania: ");
+        int szukana = scanner.nextInt();
