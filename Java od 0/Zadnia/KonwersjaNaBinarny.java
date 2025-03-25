a6d03e871ef3c54d1a1ee85d29d5e567a886e90e
KamilWojas <wojaskamil@yahoo.pl>
2025-03-25 23:20:21 +0100

    KonwersjaNaBinarny

--- Java od 0/Zadnia/KonwersjaNaBinarny.java
+++ Java od 0/Zadnia/KonwersjaNaBinarny.java
@@ -3,3 +3,11 @@ import java.util.Scanner;
 public class KonwersjaNaBinarny {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj liczbę całkowitą: ");
+        int liczba = scanner.nextInt();
+
+        if (liczba == 0) {
+            System.out.println("Binarnie: 0");
+            return;
+        }
