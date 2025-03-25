cbc8f8a4ea179cf9419d9ddae5b160be6f169b81
KamilWojas <wojaskamil@yahoo.pl>
2025-03-25 23:11:21 +0100

    Klasa produkt + konstruktor

---
+++ Java od 0/Zadnia/Product.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Product {
+    String name;
+    double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
