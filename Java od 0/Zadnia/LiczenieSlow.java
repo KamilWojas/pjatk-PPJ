b1ef4c68965dc853f49fb99111fdbf4e651ea928
KamilWojas <wojaskamil@yahoo.pl>
2025-03-25 23:18:39 +0100

    getOrDefault(slowo, 0) – zwraca wartość z mapy lub 0, jeśli brak.
    entrySet() – pozwala iterować po parach klucz–wartość.

--- Java od 0/Zadnia/LiczenieSlow.java
+++ Java od 0/Zadnia/LiczenieSlow.java
@@ -8,4 +8,17 @@ public class LiczenieSlow {
         String zdanie = scanner.nextLine().toLowerCase();
 
         String[] slowa = zdanie.split("\\s+"); 
-        Map<String, Integer> licznik = new HashMap<>(); 
+        Map<String, Integer> licznik = new HashMap<>();
+
+        for (String slowo : slowa) {
+            licznik.put(slowo, licznik.getOrDefault(slowo, 0) + 1);
+        }
+
+        System.out.println("Wystąpienia słów:");
+        for (Map.Entry<String, Integer> entry : licznik.entrySet()) {
+            System.out.println(entry.getKey() + " – " + entry.getValue() + " razy");
+        }
+
+        scanner.close();
+    }
+}
