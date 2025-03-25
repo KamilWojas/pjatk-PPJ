125876123e1ff7f22b23190444722ff8b765cff4
KamilWojas <wojaskamil@yahoo.pl>
2025-03-25 23:17:12 +0100

    LiczenieSlow

---
+++ Java od 0/Zadnia/LiczenieSlow.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class LiczenieSlow {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj zdanie: ");
+        String zdanie = scanner.nextLine().toLowerCase();
