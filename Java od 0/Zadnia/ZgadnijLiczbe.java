1a537902f3d8716ba2b7c812f5f74865e2c353be
KamilWojas <wojaskamil@yahoo.pl>
2025-03-26 23:48:49 +0100

    liczba od 1 do 100

--- Java od 0/Zadnia/ZgadnijLiczbe.java
+++ Java od 0/Zadnia/ZgadnijLiczbe.java
@@ -3,3 +3,6 @@ import java.util.Random;
 
 public class ZgadnijLiczbe {
     public static void main(String[] args) {
+        Random random = new Random();
+        int wylosowana = random.nextInt(100) + 1;
+    
