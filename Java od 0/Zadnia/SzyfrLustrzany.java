79a7c54498e348b7758926c7c3fab67c9c1bc486
KamilWojas <wojaskamil@yahoo.pl>
2025-03-26 23:47:39 +0100

    Różnica między a a z w kodzie ASCII wynosi 25.
    Dla a (97): 'z' - ('a' - 'a') = 'z'
    
    Dla b: 'z' - ('b' - 'a') = 'y' itd.
    
    To tzw. Atbash cipher – bardzo stare szyfrowanie.

--- Java od 0/Zadnia/SzyfrLustrzany.java
+++ Java od 0/Zadnia/SzyfrLustrzany.java
@@ -3,3 +3,21 @@ import java.util.Scanner;
 public class SzyfrLustrzany {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj tekst do zaszyfrowania: ");
+        String tekst = scanner.nextLine().toLowerCase();
+
+        StringBuilder wynik = new StringBuilder();
+
+        for (char znak : tekst.toCharArray()) {
+            if (znak >= 'a' && znak <= 'z') {
+                char lustrzana = (char) ('z' - (znak - 'a'));
+                wynik.append(lustrzana);
+            } else {
+                wynik.append(znak); 
+            }
+        }
+
+        System.out.println("Zaszyfrowany tekst: " + wynik);
+        scanner.close();
+    }
+}
