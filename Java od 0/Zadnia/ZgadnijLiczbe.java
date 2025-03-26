247e0a45794ee2f27bfce606aeb2ede0ca57296d
KamilWojas <wojaskamil@yahoo.pl>
2025-03-26 23:49:24 +0100

    random.nextInt(100) + 1 → losuje liczbę w zakresie 1–100.
    Pętla do while działa co najmniej raz – idealna do gier.
    
    Po każdej próbie użytkownik dostaje wskazówkę, jak zgadnąć.

--- Java od 0/Zadnia/ZgadnijLiczbe.java
+++ Java od 0/Zadnia/ZgadnijLiczbe.java
@@ -5,4 +5,27 @@ public class ZgadnijLiczbe {
     public static void main(String[] args) {
         Random random = new Random();
         int wylosowana = random.nextInt(100) + 1;
-    
+
+        Scanner scanner = new Scanner(System.in);
+        int proba;
+        int licznik = 0;
+
+        System.out.println("Zgadnij liczbę z zakresu 1–100!");
+
+        do {
+            System.out.print("Twoja próba: ");
+            proba = scanner.nextInt();
+            licznik++;
+
+            if (proba < wylosowana) {
+                System.out.println("Za mało!");
+            } else if (proba > wylosowana) {
+                System.out.println("Za dużo!");
+            } else {
+                System.out.println("Gratulacje! Trafiłeś za " + licznik + " razem!");
+            }
+        } while (proba != wylosowana);
+
+        scanner.close();
+    }
+}
