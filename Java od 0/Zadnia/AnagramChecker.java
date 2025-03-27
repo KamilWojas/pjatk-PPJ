a587da0fea86e0e93a2d3e3e624277459ad8484b
KamilWojas <wojaskamil@yahoo.pl>
2025-03-27 23:32:08 +0100

    Zadanie: Detektor anagramów

--- Java od 0/Zadnia/AnagramChecker.java
+++ Java od 0/Zadnia/AnagramChecker.java
@@ -3,3 +3,22 @@ import java.util.Scanner;
 
 public class AnagramChecker {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj pierwsze słowo: ");
+        String slowo1 = scanner.nextLine().toLowerCase();
+
+        System.out.print("Podaj drugie słowo: ");
+        String slowo2 = scanner.nextLine().toLowerCase();
+
+        boolean wynik = czyAnagram(slowo1, slowo2);
+
+        if (wynik) {
+            System.out.println("Słowa są anagramami.");
+        } else {
+            System.out.println("Słowa NIE są anagramami.");
+        }
+
+        scanner.close();
+    }
+    }
