6cf125ec212116c192ab0e5fded9c9012ca3c720
KamilWojas <wojaskamil@yahoo.pl>
2025-03-27 23:33:25 +0100

    Zadanie: Formatowanie numeru telefonu

--- Java od 0/Zadnia/FormatTelefonu.java
+++ Java od 0/Zadnia/FormatTelefonu.java
@@ -2,3 +2,8 @@ import java.util.Scanner;
 
 public class FormatTelefonu {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj 9-cyfrowy numer telefonu: ");
+        String numer = scanner.nextLine().replaceAll("\\s+", "");
+    }
