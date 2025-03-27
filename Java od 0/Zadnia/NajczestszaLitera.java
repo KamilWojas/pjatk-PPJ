883f22d499db1f31d07246f2117f77a68186728e
KamilWojas <wojaskamil@yahoo.pl>
2025-03-27 23:35:04 +0100

    Zadanie: Najczęściej występująca litera w tekście

--- Java od 0/Zadnia/NajczestszaLitera.java
+++ Java od 0/Zadnia/NajczestszaLitera.java
@@ -4,3 +4,17 @@ import java.util.Scanner;
 
 public class NajczestszaLitera {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj tekst: ");
+        String tekst = scanner.nextLine().toLowerCase();
+
+        Map<Character, Integer> licznik = new HashMap<>();
+
+        for (char znak : tekst.toCharArray()) {
+            if (Character.isLetter(znak)) {
+                licznik.put(znak, licznik.getOrDefault(znak, 0) + 1);
+            }
+        }
+
+    }
