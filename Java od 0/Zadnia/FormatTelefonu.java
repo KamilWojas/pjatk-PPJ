708c3cd5c01af9072243ad6a8bd90a7e2a2d3a36
KamilWojas <wojaskamil@yahoo.pl>
2025-03-27 23:33:50 +0100

    substring(0, 3) – wyciąga pierwsze 3 znaki.
    numer.matches("\\d{9}") – sprawdza, czy numer zawiera dokładnie 9 cyfr.
    
    replaceAll("\\s+", "") – usuwa ewentualne spacje.

--- Java od 0/Zadnia/FormatTelefonu.java
+++ Java od 0/Zadnia/FormatTelefonu.java
@@ -6,4 +6,16 @@ public class FormatTelefonu {
 
         System.out.print("Podaj 9-cyfrowy numer telefonu: ");
         String numer = scanner.nextLine().replaceAll("\\s+", "");
+        
+        if (numer.matches("\\d{9}")) {
+            String sformatowany = numer.substring(0, 3) + "-" +
+                    numer.substring(3, 6) + "-" +
+                    numer.substring(6);
+            System.out.println("Sformatowany numer: " + sformatowany);
+        } else {
+            System.out.println("Błędny numer – musi zawierać dokładnie 9 cyfr.");
+        }
+
+        scanner.close();
     }
+}
