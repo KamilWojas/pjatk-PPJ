9dfa36c015756447a2e0fc60d0e321435f3cac89
KamilWojas <wojaskamil@yahoo.pl>
2025-03-28 22:50:22 +0100

    KalkulatorGodzin

--- Java od 0/Zadnia/KalkulatorGodzin.java
+++ Java od 0/Zadnia/KalkulatorGodzin.java
@@ -11,3 +11,12 @@ public class KalkulatorGodzin {
 
         System.out.print("Podaj godzinę zakończenia pracy (HH:MM): ");
         String koniec = scanner.nextLine();
+
+        try {
+            LocalTime godzinaStart = LocalTime.parse(start);
+            LocalTime godzinaKoniec = LocalTime.parse(koniec);
+
+            if (godzinaKoniec.isBefore(godzinaStart)) {
+                System.out.println("Błąd: Nie możesz kończyć pracy przed jej rozpoczęciem!");
+                return;
+            }
