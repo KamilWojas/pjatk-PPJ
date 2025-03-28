27663694a42b50a99474440e277f7cc9b21ac885
KamilWojas <wojaskamil@yahoo.pl>
2025-03-28 22:50:10 +0100

    KalkulatorGodzin

--- Java od 0/Zadnia/KalkulatorGodzin.java
+++ Java od 0/Zadnia/KalkulatorGodzin.java
@@ -5,3 +5,9 @@ import java.util.Scanner;
 public class KalkulatorGodzin {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj godzinę rozpoczęcia pracy (HH:MM): ");
+        String start = scanner.nextLine();
+
+        System.out.print("Podaj godzinę zakończenia pracy (HH:MM): ");
+        String koniec = scanner.nextLine();
