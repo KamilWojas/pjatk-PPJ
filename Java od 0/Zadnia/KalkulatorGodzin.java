9c54aaaf422509f68939f83a6f0c297c391518de
KamilWojas <wojaskamil@yahoo.pl>
2025-03-28 22:49:56 +0100

    KalkulatorGodzin

---
+++ Java od 0/Zadnia/KalkulatorGodzin.java
@@ -0,0 +1,7 @@
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class KalkulatorGodzin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
