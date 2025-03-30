c1bc36a5b9414f8afcc41065b572b1a3a3d25925
KamilWojas <wojaskamil@yahoo.pl>
2025-03-30 18:51:37 +0200

    ListaZadan

---
+++ Java od 0/Zadnia/ListaZadan.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ListaZadan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> zadania = new ArrayList<>();
