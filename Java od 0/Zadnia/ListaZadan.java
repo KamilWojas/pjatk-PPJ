aaef37d841d9e53ea99d95d631b27b251b54ca03
KamilWojas <wojaskamil@yahoo.pl>
2025-03-30 18:55:41 +0200

    ListaZadan

--- Java od 0/Zadnia/ListaZadan.java
+++ Java od 0/Zadnia/ListaZadan.java
@@ -6,3 +6,14 @@ public class ListaZadan {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<String> zadania = new ArrayList<>();
+
+        int opcja;
+        do {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1. Dodaj zadanie");
+            System.out.println("2. Pokaż zadania");
+            System.out.println("3. Wyjście");
+            System.out.print("Wybierz opcję: ");
+
+            opcja = scanner.nextInt();
+            scanner.nextLine();
