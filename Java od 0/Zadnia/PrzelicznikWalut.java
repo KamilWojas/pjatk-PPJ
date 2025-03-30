85326ccb619e80ce92da650a8ca1fabc998728f9
KamilWojas <wojaskamil@yahoo.pl>
2025-03-30 18:57:58 +0200

    PrzelicznikWalut

--- Java od 0/Zadnia/PrzelicznikWalut.java
+++ Java od 0/Zadnia/PrzelicznikWalut.java
@@ -2,3 +2,15 @@ import java.util.Scanner;
 
 public class PrzelicznikWalut {
     public static void main(String[] args) {
+        final double KURS_EUR = 4.5;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj kwotę: ");
+        double kwota = scanner.nextDouble();
+
+        System.out.print("Wybierz kierunek konwersji (1 = PLN → EUR, 2 = EUR → PLN): ");
+        int kierunek = scanner.nextInt();
+
+        double wynik;
+    }
