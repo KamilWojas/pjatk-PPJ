f9662c07b0009d0afb70333ca02f0f4170d3f392
KamilWojas <wojaskamil@yahoo.pl>
2025-03-31 23:29:28 +0200

    ParzystaNieparzystaGra

---
+++ Java od 0/Zadnia/ParzystaNieparzystaGra.java
@@ -0,0 +1,7 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ParzystaNieparzystaGra {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
