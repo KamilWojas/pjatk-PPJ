346683203be08cb52e490d8aab9daf28834bd673
KamilWojas <wojaskamil@yahoo.pl>
2025-03-31 23:29:45 +0200

    ParzystaNieparzystaGra

--- Java od 0/Zadnia/ParzystaNieparzystaGra.java
+++ Java od 0/Zadnia/ParzystaNieparzystaGra.java
@@ -5,3 +5,6 @@ public class ParzystaNieparzystaGra {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+
+        System.out.print("Podaj liczbę od 1 do 10: ");
+        int gracz = scanner.nextInt();
