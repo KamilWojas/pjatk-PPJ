35876ab61c55b4bf2dff0a74ee74c91a9e0d1439
KamilWojas <wojaskamil@yahoo.pl>
2025-03-31 23:28:06 +0200

    KalkulatorBMI

--- Java od 0/Zadnia/KalkulatorBMI.java
+++ Java od 0/Zadnia/KalkulatorBMI.java
@@ -3,3 +3,13 @@ import java.util.Scanner;
 public class KalkulatorBMI {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj swoją wagę (w kg): ");
+        double waga = scanner.nextDouble();
+
+        System.out.print("Podaj swój wzrost (w metrach): ");
+        double wzrost = scanner.nextDouble();
+
+        double bmi = waga / (wzrost * wzrost);
+
+        System.out.printf("Twoje BMI: %.2f\n", bmi);
