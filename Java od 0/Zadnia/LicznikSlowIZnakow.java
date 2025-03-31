2c67690366e0f0dc54a5ed1a8b0f2aac9d785923
KamilWojas <wojaskamil@yahoo.pl>
2025-03-31 23:34:44 +0200

    LicznikSlowIZnakow

--- Java od 0/Zadnia/LicznikSlowIZnakow.java
+++ Java od 0/Zadnia/LicznikSlowIZnakow.java
@@ -3,3 +3,6 @@ import java.util.Scanner;
 public class LicznikSlowIZnakow {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Wpisz dowolny tekst: ");
+        String tekst = scanner.nextLine();
