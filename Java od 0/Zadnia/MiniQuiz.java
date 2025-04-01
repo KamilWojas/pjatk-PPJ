071d03e7b4a31595be504c1323094d45c567d5cf
KamilWojas <wojaskamil@yahoo.pl>
2025-04-01 23:16:15 +0200

    MiniQuiz

--- Java od 0/Zadnia/MiniQuiz.java
+++ Java od 0/Zadnia/MiniQuiz.java
@@ -4,4 +4,9 @@ public class MiniQuiz {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int punkty = 0;
-    }
+
+        System.out.println("QUIZ – Odpowiedz a, b lub c");
+
+        System.out.println("\n1. Jaki jest język urzędowy w Polsce?");
+        System.out.println("a) niemiecki\nb) polski\nc) rosyjski");
+        if (scanner.nextLine().equalsIgnoreCase("b")) punkty++;
