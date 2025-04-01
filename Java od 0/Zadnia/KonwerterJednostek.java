756d8b550004c3c46a0c95d2e7c3fad3b292224c
KamilWojas <wojaskamil@yahoo.pl>
2025-04-01 23:08:23 +0200

    KonwerterJednostek

--- Java od 0/Zadnia/KonwerterJednostek.java
+++ Java od 0/Zadnia/KonwerterJednostek.java
@@ -3,3 +3,8 @@ import java.util.Scanner;
 public class KonwerterJednostek {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("--- KONWERTER JEDNOSTEK ---");
+        System.out.print("Podaj wartość (liczba): ");
+        double wartosc = scanner.nextDouble();
+        scanner.nextLine(); 
