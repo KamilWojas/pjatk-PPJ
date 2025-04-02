8c0364db3c7d9f814689fbe06025511fe36d359d
KamilWojas <wojaskamil@yahoo.pl>
2025-04-02 23:27:32 +0200

    Employee

--- Java od 0/Zadania12/Employee.java
+++ Java od 0/Zadania12/Employee.java
@@ -19,3 +19,24 @@ class Employee {
         return name + " – " + String.format("%.2f", salary);
     }
 }
+
+public class SystemPracownikow {
+    public static void main(String[] args) {
+
+        List<Employee> pracownicy = new ArrayList<>();
+        pracownicy.add(new Employee(1, "Anna Nowak", "HR", 5000));
+        pracownicy.add(new Employee(2, "Jan Kowalski", "IT", 6500));
+        pracownicy.add(new Employee(3, "Kamil Zieliński", "HR", 4700));
+        pracownicy.add(new Employee(4, "Marta Wiśniewska", "IT", 7100));
+        pracownicy.add(new Employee(5, "Piotr Maj", "Marketing", 4300));
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Podaj nazwę działu: ");
+        String dzial = scanner.nextLine();
+
+        double suma = 0;
+        int licznik = 0;
+
+        System.out.println("Pracownicy działu " + dzial + ":");
