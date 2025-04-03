c602a84e61b911f6710639a353abe856e9c2a082
KamilWojas <wojaskamil@yahoo.pl>
2025-04-03 23:32:05 +0200

    Zadanie: Sprawdź, czy liczba jest palindromem (rekurencyjnie)

--- Java od 0/Zadnia/PalindromLiczby.java
+++ Java od 0/Zadnia/PalindromLiczby.java
@@ -2,3 +2,7 @@ import java.util.Scanner;
 
 public class PalindromLiczby {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj liczbę: ");
+        String liczba = scanner.nextLine();
+    
