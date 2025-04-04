6732722e1e51cdd4dae333172e89dbd1ec7cad7e
KamilWojas <wojaskamil@yahoo.pl>
2025-04-04 23:23:00 +0200

    User

--- Java od 0/Zadnia/User.java
+++ Java od 0/Zadnia/User.java
@@ -3,3 +3,8 @@ import java.util.Scanner;
 class User {
     String username;
     String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
