2abc1c853afb0c7e18c3f4db8fde487aaf19c726
KamilWojas <wojaskamil@yahoo.pl>
2025-04-04 23:21:10 +0200

    WalidatorHasla

--- Java od 0/Zadnia/WalidatorHasla.java
+++ Java od 0/Zadnia/WalidatorHasla.java
@@ -2,3 +2,8 @@ import java.util.Scanner;
 
 public class WalidatorHasla {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj hasło: ");
+        String haslo = scanner.nextLine();
+    }
