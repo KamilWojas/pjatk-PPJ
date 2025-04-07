2daa2847feaab6b5b08ef28ebb9265965ed884fc
KamilWojas <wojaskamil@yahoo.pl>
2025-04-07 23:02:55 +0200

    Co wypisze kod?

--- Java od 0/Zadnia/Test.java
+++ Java od 0/Zadnia/Test.java
@@ -13,3 +13,15 @@ public class Main {
 //Rozwiązanie:
 //   Konstruktor Test() uruchamia się automatycznie przy tworzeniu obiektu new Test();
 //   Wypisze: Konstruktor!
+
+
+
+==========================================================================================
+//Co wypisze kod?
+        
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        for (int i = 0; i < arr.length / 2; i++) {
+        System.out.print(
+        arr[i % 3 == 0 ? arr.length - 1 - i : i]
+        );
+        }
