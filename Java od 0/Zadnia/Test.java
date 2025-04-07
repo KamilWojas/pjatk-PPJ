ca37bd056fb3e6bdfc6cf0ba3c06f0e749e4bedd
KamilWojas <wojaskamil@yahoo.pl>
2025-04-07 23:12:20 +0200

    Tablice i operatory warunkowe

--- Java od 0/Zadnia/Test.java
+++ Java od 0/Zadnia/Test.java
@@ -124,3 +124,16 @@ class B extends A {
     @Override
     void show() { System.out.println("B"); }
 }
+
+
+==========================================================================================
+
+//Tablice i operatory warunkowe
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (int i = 0; i < arr.length / 2; i++) {
+        System.out.print(
+        arr[i % 3 == 0 ? arr.length - 1 - i : i]
+        );
+        }
