d998fe68835cfa1740618ef1dbd252e7ea2b09c3
KamilWojas <wojaskamil@yahoo.pl>
2025-04-09 23:34:31 +0200

    MenuKalkulator

--- Java od 0/Zadnia/MenuKalkulator.java
+++ Java od 0/Zadnia/MenuKalkulator.java
@@ -2,3 +2,12 @@ import java.util.Scanner;
 
 public class MenuKalkulator {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String wybor;
+
+        do {
+            System.out.println("\n--- KALKULATOR ---");
+            System.out.println("Dostępne działania: dodaj, odejmij, pomnóż, podziel, wyjście");
+            System.out.print("Wybierz działanie: ");
+            wybor = scanner.nextLine().toLowerCase();
+    }
