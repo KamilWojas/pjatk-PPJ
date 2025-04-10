cbcb6eb4a4a2a7f7337b5570426517eb2425f18e
KamilWojas <wojaskamil@yahoo.pl>
2025-04-10 23:31:49 +0200

    ZapisDoPliku

--- Java od 0/Zadnia/ZapisDoPliku.java
+++ Java od 0/Zadnia/ZapisDoPliku.java
@@ -4,3 +4,7 @@ import java.util.Scanner;
 
 public class ZapisDoPliku {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Wpisz tekst do zapisania (wpisz 'KONIEC' aby zakończyć):");
+    }
