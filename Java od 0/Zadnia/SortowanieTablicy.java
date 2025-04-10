39a761f9e5c6a2fd283c7f3dbc4a1f7a4ef8c2a7
KamilWojas <wojaskamil@yahoo.pl>
2025-04-10 23:29:59 +0200

    SortowanieTablicy

--- Java od 0/Zadnia/SortowanieTablicy.java
+++ Java od 0/Zadnia/SortowanieTablicy.java
@@ -4,3 +4,16 @@ import java.util.Scanner;
 
 public class SortowanieTablicy {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ile liczb chcesz wprowadzić? ");
+        int n = scanner.nextInt();
+
+        Integer[] liczby = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Podaj liczbę " + (i + 1) + ": ");
+            liczby[i] = scanner.nextInt();
+        }
+
+    }
