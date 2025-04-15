864b89aac9b5d59db52a6af2c6464fd99801574b
KamilWojas <wojaskamil@yahoo.pl>
2025-04-15 23:34:13 +0200

    SystemLogowania

---
+++ Java od 0/Zadnia/SystemLogowania.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class SystemLogowania {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
