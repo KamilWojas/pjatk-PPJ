b9c8570e620de8a8bdae527f937391efabbee67e
KamilWojas <wojaskamil@yahoo.pl>
2025-04-15 23:31:38 +0200

    SystemLogowania

--- Java od 0/Zadania12/SystemLogowania.java
+++ Java od 0/Zadania12/SystemLogowania.java
@@ -4,3 +4,14 @@ import java.util.Scanner;
 
 public class SystemLogowania {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, String> uzytkownicy = new HashMap<>();
+
+        while (true) {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1. Rejestracja");
+            System.out.println("2. Logowanie");
+            System.out.println("3. Wyjście");
+            System.out.print("Wybierz opcję: ");
+            String wybor = scanner.nextLine();
+    }
