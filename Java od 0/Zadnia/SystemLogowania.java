72643097bca58ec1c49128b5aa84fcb2d0611dc2
KamilWojas <wojaskamil@yahoo.pl>
2025-04-15 23:34:34 +0200

    Utworzenie mapy login–hasło

--- Java od 0/Zadnia/SystemLogowania.java
+++ Java od 0/Zadnia/SystemLogowania.java
@@ -5,3 +5,11 @@ import java.util.Scanner;
 public class SystemLogowania {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> konta = new HashMap<>();
+        konta.put("admin", "1234");
+        konta.put("user", "haslo");
+        konta.put("goscia", "guest");
+
+        int proby = 0;
+        boolean zalogowano = false;
