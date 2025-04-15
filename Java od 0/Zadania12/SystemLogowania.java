8f4018ff7c898d762b87e27b5dfb958a2166c896
KamilWojas <wojaskamil@yahoo.pl>
2025-04-15 23:32:08 +0200

    SystemLogowania

--- Java od 0/Zadania12/SystemLogowania.java
+++ Java od 0/Zadania12/SystemLogowania.java
@@ -14,4 +14,18 @@ public class SystemLogowania {
             System.out.println("3. Wyjście");
             System.out.print("Wybierz opcję: ");
             String wybor = scanner.nextLine();
-    }
+
+            switch (wybor) {
+                case "1":
+                    System.out.print("Podaj login: ");
+                    String nowyLogin = scanner.nextLine();
+
+                    if (uzytkownicy.containsKey(nowyLogin)) {
+                        System.out.println("Taki login już istnieje.");
+                    } else {
+                        System.out.print("Podaj hasło: ");
+                        String haslo = scanner.nextLine();
+                        uzytkownicy.put(nowyLogin, haslo);
+                        System.out.println("Zarejestrowano pomyślnie!");
+                    }
+                    break;
