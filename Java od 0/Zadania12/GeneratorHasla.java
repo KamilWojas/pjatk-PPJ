164aaa8d08bf526927302aba93b19fb073ce2618
KamilWojas <wojaskamil@yahoo.pl>
2025-04-19 20:54:12 +0200

    GeneratorHasla

--- Java od 0/Zadania12/GeneratorHasla.java
+++ Java od 0/Zadania12/GeneratorHasla.java
@@ -4,3 +4,15 @@ import java.util.Scanner;
 public class GeneratorHasla {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        final String znaki = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+        SecureRandom random = new SecureRandom();
+
+        System.out.print("Podaj długość hasła: ");
+        int dlugosc = scanner.nextInt();
+
+        if (dlugosc < 6) {
+            System.out.println("Hasło powinno mieć co najmniej 6 znaków.");
+            scanner.close();
+            return;
+        }
