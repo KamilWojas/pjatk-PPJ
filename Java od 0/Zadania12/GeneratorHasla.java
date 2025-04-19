64e29276564c8f641a3afd3eec6425e48bbcd3f8
KamilWojas <wojaskamil@yahoo.pl>
2025-04-19 20:53:57 +0200

    GeneratorHasla

---
+++ Java od 0/Zadania12/GeneratorHasla.java
@@ -0,0 +1,6 @@
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class GeneratorHasla {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
