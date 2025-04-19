4e694946945f38bac2beb1200d13360cf2a8e7c0
KamilWojas <wojaskamil@yahoo.pl>
2025-04-19 20:51:42 +0200

    User

--- Java od 0/Zadania12/User.java
+++ Java od 0/Zadania12/User.java
@@ -23,3 +23,11 @@ public class ListaUzytkownikow {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<User> users = new ArrayList<>();
+
+        while (true) {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1. Dodaj użytkownika");
+            System.out.println("2. Wyświetl wszystkich użytkowników");
+            System.out.println("3. Wyjdź");
+            System.out.print("Wybierz opcję: ");
+            String wybor = scanner.nextLine();
