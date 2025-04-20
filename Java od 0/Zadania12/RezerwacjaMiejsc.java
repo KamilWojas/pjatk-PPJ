38c30ed4471aa4cafefb2491e90a4d3cecdefc81
KamilWojas <wojaskamil@yahoo.pl>
2025-04-20 10:28:35 +0200

    wybór

--- Java od 0/Zadania12/RezerwacjaMiejsc.java
+++ Java od 0/Zadania12/RezerwacjaMiejsc.java
@@ -18,3 +18,16 @@ public class RezerwacjaMiejsc {
             System.out.println("3. Wyjdź");
             System.out.print("Wybierz opcję: ");
             String wybor = scanner.nextLine();
+
+            switch (wybor) {
+                case "1":
+                    System.out.println("Stan miejsc (O = wolne, X = zajęte):");
+                    for (int i = 0; i < sala.length; i++) {
+                        System.out.print("Rząd " + (i + 1) + ": ");
+                        for (int j = 0; j < sala[i].length; j++) {
+                            System.out.print(sala[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+
