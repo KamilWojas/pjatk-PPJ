9c229051f8bb14f864e598481100af7a90ab5fcc
KamilWojas <wojaskamil@yahoo.pl>
2025-04-20 10:27:39 +0200

    Inicjalizacja miejsc jako wolnych ('O')

--- Java od 0/Zadania12/RezerwacjaMiejsc.java
+++ Java od 0/Zadania12/RezerwacjaMiejsc.java
@@ -4,3 +4,9 @@ public class RezerwacjaMiejsc {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char[][] sala = new char[5][6];
+
+        for (int i = 0; i < sala.length; i++) {
+            for (int j = 0; j < sala[i].length; j++) {
+                sala[i][j] = 'O';
+            }
+        }
