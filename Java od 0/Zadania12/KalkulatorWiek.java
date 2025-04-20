b0da1b0142c5201db0067f3002e78adaa3a21526
KamilWojas <wojaskamil@yahoo.pl>
2025-04-20 10:30:01 +0200

    wybór

---
+++ Java od 0/Zadania12/KalkulatorWiek.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+public class KalkulatorWiek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
