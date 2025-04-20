392d85bff9ae2483ed0d44bc273dd16ea741fa3e
KamilWojas <wojaskamil@yahoo.pl>
2025-04-20 10:30:25 +0200

    KalkulatorWiek

--- Java od 0/Zadania12/KalkulatorWiek.java
+++ Java od 0/Zadania12/KalkulatorWiek.java
@@ -4,4 +4,7 @@ import java.util.Scanner;
 
 public class KalkulatorWiek {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj swoją datę urodzenia (RRRR-MM-DD): ");
+        String dataTekst = scanner.nextLine();
