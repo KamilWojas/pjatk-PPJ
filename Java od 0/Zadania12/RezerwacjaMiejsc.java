392f0a6ecc1fd55eaac8e52b6cea6e5c7d5e5308
KamilWojas <wojaskamil@yahoo.pl>
2025-04-20 10:27:10 +0200

    5 rzędów x 6 miejsc

---
+++ Java od 0/Zadania12/RezerwacjaMiejsc.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class RezerwacjaMiejsc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[][] sala = new char[5][6];
