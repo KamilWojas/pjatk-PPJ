23d7b08bc82343bfde5bf902f9a2b0b3343b71ef
KamilWojas <wojaskamil@yahoo.pl>
2025-04-21 10:45:45 +0200

    Pętla for po literach alfabetu

--- Java od 0/Zadania12/LicznikLiter.java
+++ Java od 0/Zadania12/LicznikLiter.java
@@ -16,3 +16,13 @@ public class LicznikLiter {
                 mapa.put(c, mapa.getOrDefault(c, 0) + 1);
             }
         }
+
+        System.out.println("\nCzęstość występowania liter:");
+        for (char litera = 'a'; litera <= 'z'; litera++) {
+            int ile = mapa.getOrDefault(litera, 0);
+            System.out.println(litera + " – " + ile + " raz(y)");
+        }
+
+        scanner.close();
+    }
+}
