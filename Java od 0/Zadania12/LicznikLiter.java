0cde06feb1deb24b2b20d6b4ee8ecddc297958e3
KamilWojas <wojaskamil@yahoo.pl>
2025-04-21 10:43:44 +0200

    Prosimy o wpisanie tekstu

--- Java od 0/Zadania12/LicznikLiter.java
+++ Java od 0/Zadania12/LicznikLiter.java
@@ -3,4 +3,8 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class LicznikLiter {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.print("Wpisz tekst: ");
+        String tekst = scanner.nextLine().toLowerCase();
