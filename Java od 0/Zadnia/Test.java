4d95a690600b8ae6f3db7a9faaf7f3ee007aa148
KamilWojas <wojaskamil@yahoo.pl>
2025-05-04 21:17:04 +0200

    Utwórz dwuwymiarową tablicę o rozmiarach 8×8 i wypełnij ją losowymi wartościami z przedziału od 0 do 10. Następnie sprawdź, czy na przekątnych tej tablicy (łącznie) występują przynajmniej 3 takie same liczby.

--- Java od 0/Zadnia/Test.java
+++ Java od 0/Zadnia/Test.java
@@ -1290,3 +1290,8 @@ public class Przekatne8x8 {
 ==========================================================================================
 //Utwórz dwuwymiarową tablicę o rozmiarach 8×8 i wypełnij ją losowymi wartościami z przedziału od 0 do 10. 
 // Następnie sprawdź, czy na przekątnych tej tablicy (łącznie) występują przynajmniej 3 takie same liczby.
+
+public class PrzekatneTablicy {
+    public static void main(String[] args) {
+        int[][] tablica = new int[8][8];
+        Random rand = new Random();
