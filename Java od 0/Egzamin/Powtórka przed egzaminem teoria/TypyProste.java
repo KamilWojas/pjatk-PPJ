a6ff0fe1e6d95d8ed0d7f6c894978d53ed1f5b16
KamilWojas <wojaskamil@yahoo.pl>
2025-06-16 23:41:51 +0200

    boolean: tylko true/false

---
+++ Java od 0/Egzamin/Powtórka przed egzaminem teoria/TypyProste.java
@@ -0,0 +1,8 @@
+public class TypyProste {
+    public static void main(String[] args) {
+
+        //boolean: tylko true/false
+        boolean wartoscLogiczna1 = true;
+        boolean wartoscLogiczna2 = false;
+    }
+}
