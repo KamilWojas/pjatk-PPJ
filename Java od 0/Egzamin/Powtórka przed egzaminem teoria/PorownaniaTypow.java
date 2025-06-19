bac2bd7f9eafe6a18443fe82711945843e7dfc36
KamilWojas <wojaskamil@yahoo.pl>
2025-06-19 23:20:52 +0200

    Liczby całkowite (int, byte, short, long)

--- Java od 0/Egzamin/Powtórka przed egzaminem teoria/PorownaniaTypow.java
+++ Java od 0/Egzamin/Powtórka przed egzaminem teoria/PorownaniaTypow.java
@@ -1,6 +1,13 @@
 public class PorownaniaTypow {
     public static void main(String[] args) {
 
+        //Typ logiczny (boolean)
         boolean logiczna1 = true;
         boolean logiczna2 = false;
+
+        //Liczby całkowite (int, byte, short, long)
+        int liczbaCalkowita = 10;
+        byte liczbaByte = 10;
+        short liczbaShort = 10;
+        long liczbaLong = 10L;
     }
