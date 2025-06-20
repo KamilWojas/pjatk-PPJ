022e151597f91cc104d25a6a529350fb41e8d461
KamilWojas <wojaskamil@yahoo.pl>
2025-06-20 21:58:00 +0200

    Zadeklarować zmienne typu int z nazwami, które łamią lub nie łamią zasad języka Java. Jeśli nazwa zmiennej jest nieprawidłowa, musimy ją zakomentować (czyli oznaczyć jako komentarz //).

---
+++ Java od 0/Egzamin/Powtórka przed egzaminem teoria/ZmienneTest.java
@@ -0,0 +1,3 @@
+//Cel zadania
+//Zadeklarować zmienne typu int z nazwami, które łamią lub nie łamią zasad języka Java.
+//Jeśli nazwa zmiennej jest nieprawidłowa, musimy ją zakomentować (czyli oznaczyć jako komentarz //).
